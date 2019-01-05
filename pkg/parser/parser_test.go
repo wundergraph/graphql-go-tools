1b608b3ea71d50a0782472ff034f91415c12d82e
jnsone11 <jens.neuse@gmx.de>
2019-01-05 16:56:04 +0100

    remove unused code

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/sebdah/goldie"
 	"io/ioutil"
 	"log"
-	"runtime"
 	"strings"
 	"testing"
 )
@@ -188,18 +187,4 @@ func BenchmarkParser(b *testing.B) {
 
 	}
 
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
