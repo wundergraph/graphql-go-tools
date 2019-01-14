406018919025f172e9142b06c38fa4241deca239
jnsone11 <jens.neuse@gmx.de>
2019-01-14 20:50:14 +0100

    fix printing of golden fixtures

--- pkg/document/byteslice.go
+++ pkg/document/byteslice.go
@@ -1,3 +1,9 @@
 package document
 
+import "github.com/jensneuse/graphql-go-tools/pkg/lexing/literal"
+
 type ByteSlice []byte
+
+func (b ByteSlice) MarshalJSON() ([]byte, error) {
+	return append(append(literal.QUOTE, b...), literal.QUOTE...), nil
+}
