6629b961f3525f4be592a5934fa5fc6bf5b427e7
jnsone11 <jens.neuse@gmx.de>
2019-01-16 07:33:33 +0100

    add LineStart, LineEnd to Position

--- pkg/lexing/position/position.go.go
+++ pkg/lexing/position/position.go.go
@@ -3,10 +3,17 @@ package position
 import "fmt"
 
 type Position struct {
-	Line int
-	Char int
+	LineStart int
+	LineEnd   int
+	CharStart int
+	CharEnd   int
 }
 
 func (p Position) String() string {
-	return fmt.Sprintf("%d:%d", p.Line, p.Char)
+	return fmt.Sprintf("%d:%d-%d:%d", p.LineStart, p.CharStart, p.LineEnd, p.CharEnd)
+}
+
+func (p *Position) SetEnd(position Position) {
+	p.LineEnd = position.LineEnd
+	p.CharEnd = position.CharEnd
 }
