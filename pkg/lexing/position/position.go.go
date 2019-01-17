f02cdf8235548b30788d0f50886de95f71ae138e
jnsone11 <jens.neuse@gmx.de>
2019-01-17 22:18:22 +0100

    remove SetPosition from Position, this method moved to token.Token

--- pkg/lexing/position/position.go.go
+++ pkg/lexing/position/position.go.go
@@ -12,8 +12,3 @@ type Position struct {
 func (p Position) String() string {
 	return fmt.Sprintf("%d:%d-%d:%d", p.LineStart, p.CharStart, p.LineEnd, p.CharEnd)
 }
-
-func (p *Position) SetEnd(position Position) {
-	p.LineEnd = position.LineEnd
-	p.CharEnd = position.CharEnd
-}
