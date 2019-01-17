006af33c59ab486e8a686a009565930354013d97
jnsone11 <jens.neuse@gmx.de>
2019-01-17 22:19:32 +0100

    add SetStart and SetEnd methods to token.Token

--- pkg/lexing/token/token.go
+++ pkg/lexing/token/token.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"github.com/jensneuse/graphql-go-tools/pkg/document"
 	"github.com/jensneuse/graphql-go-tools/pkg/lexing/keyword"
-	"github.com/jensneuse/graphql-go-tools/pkg/lexing/literal"
 	"github.com/jensneuse/graphql-go-tools/pkg/lexing/position"
 )
 
 type Token struct {
-	Keyword     keyword.Keyword
-	Literal     document.ByteSlice
-	Position    position.Position
-	Description string
+	Keyword      keyword.Keyword
+	Literal      document.ByteSliceReference
+	TextPosition position.Position
+	Description  string
 }
 
 func (t Token) String() string {
-	return fmt.Sprintf("Token:: Keyword: %s, Literal: %s Pos: %s", t.Keyword, t.Literal, t.Position)
+	return fmt.Sprintf("Token:: Keyword: %s, Pos: %s", t.Keyword, t.TextPosition)
 }
 
-var (
+func (t *Token) SetStart(inputPosition int, textPosition position.Position) {
+	t.Literal.Start = uint16(inputPosition)
+	t.TextPosition.LineStart = textPosition.LineStart
+	t.TextPosition.CharStart = textPosition.CharStart
+}
+
+func (t *Token) SetEnd(inputPosition int, textPosition position.Position) {
+	t.Literal.End = uint16(inputPosition)
+	t.TextPosition.LineEnd = textPosition.LineStart
+	t.TextPosition.CharEnd = textPosition.CharStart
+}
+
+/*var (
 	EOF = Token{
 		Keyword: keyword.EOF,
 		Literal: literal.EOF,
@@ -81,3 +92,4 @@ var (
 		Literal: literal.AND,
 	}
 )
+*/
