0644d57d93d21043de73e3898d60d38ab1b19fb6
jensneuse <jens.neuse@gmx.de>
2019-01-18 08:37:04 +0100

    remove unsued code

--- pkg/document/typedefinition.go
+++
@@ -1,9 +0,0 @@
-package document
-
-// TypeDefinition as specified in:
-// http://facebook.github.io/graphql/draft/#TypeDefinition
-type TypeDefinition struct {
-	Description      ByteSliceReference
-	Name             ByteSliceReference
-	FieldsDefinition FieldDefinitions
-}
