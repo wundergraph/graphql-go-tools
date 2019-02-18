04caf4b1bb9ff8355ba185d03f3734701e290ef8
jnsone11 <jens.neuse@gmx.de>
2019-02-18 08:28:58 +0100

    add lone anonymous operation validation implementation

---
+++ pkg/validation/rules/execution/lone_anonymous_operation.go
@@ -0,0 +1,27 @@
+package execution
+
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/lookup"
+	"github.com/jensneuse/graphql-go-tools/pkg/validation"
+	"github.com/jensneuse/graphql-go-tools/pkg/validation/rules"
+)
+
+// LoneAnonymousOperation
+// https://facebook.github.io/graphql/draft/#sec-Lone-Anonymous-Operation
+func LoneAnonymousOperation() rules.ExecutionRule {
+	return func(l *lookup.Lookup, w *lookup.Walker) validation.Result {
+
+		definitions := l.OperationDefinitions()
+		if len(definitions) <= 1 {
+			return validation.Valid()
+		}
+
+		for _, definition := range definitions {
+			if definition.Name == -1 {
+				return validation.Invalid(validation.LoneAnonymousOperation, validation.AnonymousOperationMustBeLonePerDocument, definition.Position, definition.Name)
+			}
+		}
+
+		return validation.Valid()
+	}
+}
