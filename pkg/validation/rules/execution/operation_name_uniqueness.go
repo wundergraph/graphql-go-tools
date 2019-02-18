0c6c3078da3e362192af5756f3f5b34f20021465
jnsone11 <jens.neuse@gmx.de>
2019-02-18 08:29:10 +0100

    add operation name uniqueness validation implementation

---
+++ pkg/validation/rules/execution/operation_name_uniqueness.go
@@ -0,0 +1,26 @@
+package execution
+
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/lookup"
+	"github.com/jensneuse/graphql-go-tools/pkg/validation"
+	"github.com/jensneuse/graphql-go-tools/pkg/validation/rules"
+)
+
+// OperationNameUniqueness
+// https://facebook.github.io/graphql/draft/#sec-Operation-Name-Uniqueness
+func OperationNameUniqueness() rules.ExecutionRule {
+	return func(l *lookup.Lookup, w *lookup.Walker) validation.Result {
+
+		definitions := l.OperationDefinitions()
+
+		for i, first := range definitions {
+			for k, second := range definitions {
+				if i != k && first.Name == second.Name {
+					return validation.Invalid(validation.OperationNameUniqueness, validation.OperationNameMustBeUnique, first.Position, first.Name)
+				}
+			}
+		}
+
+		return validation.Valid()
+	}
+}
