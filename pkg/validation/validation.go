69da79bc5cf993bc77b012cd8258b190d2fceafa
jnsone11 <jens.neuse@gmx.de>
2019-02-18 08:30:36 +0100

    add validation result enums and helper methods

---
+++ pkg/validation/validation.go
@@ -0,0 +1,99 @@
+//go:generate go-enum -f=$GOFILE --noprefix
+package validation
+
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/lexing/position"
+)
+
+func Valid() Result {
+	return Result{
+		Valid: true,
+	}
+}
+
+func Invalid(ruleName RuleName, description Description, subjectPosition position.Position, subjectNameRef int) Result {
+	return Result{
+		Valid:       false,
+		RuleName:    ruleName,
+		Description: description,
+		Meta: Meta{
+			SubjectPosition: subjectPosition,
+			SubjectNameRef:  subjectNameRef,
+		},
+	}
+}
+
+type Result struct {
+	Valid       bool
+	RuleName    RuleName
+	Description Description
+	Meta        Meta
+}
+
+type Meta struct {
+	SubjectPosition position.Position
+	SubjectNameRef  int
+}
+
+/*
+ENUM(
+NoRule
+ArgumentUniqueness
+DirectivesAreDefined
+DirectivesAreInValidLocations
+DirectivesAreUniquePerLocation
+FieldSelectionMerging
+FieldSelections
+Fragments
+LoneAnonymousOperation
+OperationNameUniqueness
+RequiredArguments
+SubscriptionSingleRootField
+ValidArguments
+Values
+VariableUniqueness
+VariablesAreInputTypes
+AllVariablesUsed
+AllVariableUsesDefined
+)
+*/
+type RuleName int
+
+/*
+ENUM(
+NoDescription
+AnonymousOperationMustBeLonePerDocument
+ArgumentNotDefined
+ArgumentMustBeUnique
+ArgumentRequired
+ArgumentsDefinitionMissing
+ArgumentMismatch
+DirectiveNotDefined
+DirectiveLocationInvalid
+DirectiveMustBeUniquePerLocation
+FieldNameOrAliasMismatch
+FieldSelectionsInvalid
+FragmentNotDefined
+FragmentSpreadCyclicReference
+FragmentDefinitionOnLeafNode
+FragmentRedeclared
+FragmentDeclaredButNeverUsed
+InlineFragmentOnLeafNode
+InputValueNotDefined
+MustHaveTypeCondition
+MustHaveNamedTypeCondition
+OperationNameMustBeUnique
+QueryObjectTypeNotDefined
+RootTypeNotDefined
+SelectionSetInvalid
+SelectionSetResponseShapesCannotMerge
+SubscriptionsMustHaveMaxOneRootField
+TypeNotDefined
+ValueInvalid
+VariableMustBeUniquePerOperation
+VariableMustBeValidInputType
+VariableNotDefined
+VariableDefinedButNotUsed
+)
+*/
+type Description int
