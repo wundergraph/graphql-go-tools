6a6cbe57fd785f1663fbff2f7ebc8b47e4740958
jensneuse <jens.neuse@gmx.de>
2019-02-19 08:14:28 +0100

    remove unnecessary check

--- pkg/validation/rules/execution/fragments.go
+++ pkg/validation/rules/execution/fragments.go
@@ -1,7 +1,6 @@
 package execution
 
 import (
-	"github.com/jensneuse/graphql-go-tools/pkg/document"
 	"github.com/jensneuse/graphql-go-tools/pkg/lookup"
 	"github.com/jensneuse/graphql-go-tools/pkg/validation"
 	"github.com/jensneuse/graphql-go-tools/pkg/validation/rules"
@@ -16,9 +15,6 @@ func Fragments() rules.ExecutionRule {
 		for i, definition := range definitions {
 
 			typeCondition := l.Type(definition.TypeCondition)
-			if typeCondition.Kind != document.TypeKindNAMED {
-				return validation.Invalid(validation.Fragments, validation.MustHaveNamedTypeCondition, definition.Position, definition.FragmentName)
-			}
 			if l.TypeIsScalarOrEnum(typeCondition.Name) {
 				return validation.Invalid(validation.Fragments, validation.FragmentDefinitionOnLeafNode, definition.Position, definition.FragmentName)
 			}
