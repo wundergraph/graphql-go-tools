d42f513d4764e76f349c47000e2eb39f519d8016
Jens Neuse <jens.neuse@gmx.de>
2019-03-02 20:51:15 +0100

    introduce proxy struct/api to organize everything around proxying requests

---
+++ pkg/proxy/proxy.go
@@ -0,0 +1,40 @@
+package proxy
+
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/lookup"
+	"github.com/jensneuse/graphql-go-tools/pkg/parser"
+)
+
+type Proxy struct {
+	l           *lookup.Lookup
+	p           *parser.Parser
+	w           *lookup.Walker
+	mod         *parser.ManualAstMod
+	middleWares []GraphqlMiddleware
+}
+
+type GraphqlMiddleware interface {
+	OnRequest(l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod)
+}
+
+func NewProxy(mod *parser.ManualAstMod) *Proxy {
+	return &Proxy{
+		mod: mod,
+	}
+}
+
+func (p *Proxy) SetMiddleWares(middleWares ...GraphqlMiddleware) {
+	p.middleWares = middleWares
+}
+
+func (p *Proxy) SetInput(l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) {
+	p.p = parser
+	p.l = l
+	p.w = w
+}
+
+func (p *Proxy) OnQuery() {
+	for i := range p.middleWares {
+		p.middleWares[i].OnRequest(p.l, p.w, p.p, p.mod)
+	}
+}
