1a35f21d360d5b03ba24048babee76408e0b6da3
jensneuse <jens.neuse@gmx.de>
2019-03-07 09:18:46 +0100

    add funcs to manual ast mod to set the schema types manually

--- pkg/parser/manual_ast_mod.go
+++ pkg/parser/manual_ast_mod.go
@@ -15,6 +15,29 @@ func NewManualAstMod(p *Parser) *ManualAstMod {
 	}
 }
 
+func (m *ManualAstMod) SetQueryTypeName(name int) {
+	m.p.ParsedDefinitions.TypeSystemDefinition.SchemaDefinition.Query = name
+}
+
+func (m *ManualAstMod) SetMutationTypeName(name int) {
+	m.p.ParsedDefinitions.TypeSystemDefinition.SchemaDefinition.Mutation = name
+}
+
+func (m *ManualAstMod) SetSubscriptionTypeName(name int) {
+	m.p.ParsedDefinitions.TypeSystemDefinition.SchemaDefinition.Subscription = name
+}
+
+func (m *ManualAstMod) PutLiteralString(literal string) (ref int, err error) {
+	err = m.p.l.AppendBytes([]byte(literal))
+	if err != nil {
+		return
+	}
+
+	ref = m.p.putByteSliceReference(m.p.l.Read().Literal)
+
+	return
+}
+
 func (m *ManualAstMod) PutField(field document.Field) int {
 	m.p.ParsedDefinitions.Fields = append(m.p.ParsedDefinitions.Fields, field)
 	return len(m.p.ParsedDefinitions.Fields) - 1
