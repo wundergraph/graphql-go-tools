e00662ba9a0c6c0d063ded54ce46949902171307
Jens Neuse <jens.neuse@gmx.de>
2019-03-09 17:34:11 +0100

    add schema provider to make schema fetching generic

---
+++ pkg/proxy/http/handler/schema_provider.go
@@ -0,0 +1,22 @@
+package handler
+
+// SchemaProvider exists because it's not usually the case for the handler to keep the schema around
+// Think multi tenant SaaS applications where a handler might handle schemas for many tenants
+// In case you just want to use one single schema simply use StaticSchemaProvider
+type SchemaProvider interface {
+	GetSchema(url string, schema *[]byte)
+}
+
+type StaticSchemaProvider struct {
+	schema []byte
+}
+
+func (s *StaticSchemaProvider) GetSchema(url string, schema *[]byte) {
+	*schema = s.schema
+}
+
+func NewStaticSchemaProvider(schema []byte) *StaticSchemaProvider {
+	return &StaticSchemaProvider{
+		schema: schema,
+	}
+}
