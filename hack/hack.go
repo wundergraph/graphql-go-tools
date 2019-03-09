00c1f4a68c9693a3826d7af357378a72dcd10287
Jens Neuse <jens.neuse@gmx.de>
2019-03-09 17:29:04 +0100

    add package hack explanation

---
+++ hack/hack.go
@@ -0,0 +1,5 @@
+package hack
+
+// package hack contains not yet finished parts of the library
+// the contents should still be part of the main codebase to keep the discussion going
+// but we'll keep it separated from the main codebase
