d45b833c979ce20599248ccf0af143a65b810edb
Jens Neuse <jens.neuse@gmx.de>
2019-03-10 12:10:18 +0100

    add context to graphql middleware

--- middleware/graphql_middleware.go
+++ middleware/graphql_middleware.go
@@ -1,11 +1,12 @@
 package middleware
 
 import (
+	"context"
 	"github.com/jensneuse/graphql-go-tools/pkg/lookup"
 	"github.com/jensneuse/graphql-go-tools/pkg/parser"
 )
 
 type GraphqlMiddleware interface {
-	OnRequest(l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error
-	OnResponse(response *[]byte, l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error
+	OnRequest(context context.Context, l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error
+	OnResponse(context context.Context, response *[]byte, l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error
 }
