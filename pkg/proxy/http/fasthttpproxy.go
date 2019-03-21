cfa780dae3977c5c69f641e9bc77461598b33225
jensneuse <jens.neuse@gmx.de>
2019-03-21 07:50:19 +0100

    refactor fasthttpproxy

--- pkg/proxy/http/fasthttpproxy.go
+++ pkg/proxy/http/fasthttpproxy.go
@@ -2,10 +2,10 @@ package http
 
 import (
 	"bytes"
-	"context"
-	"encoding/json"
 	"github.com/jensneuse/graphql-go-tools/pkg/middleware"
 	"github.com/jensneuse/graphql-go-tools/pkg/proxy"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
 	"github.com/valyala/fasthttp"
 	"io"
 	"net/http"
@@ -15,7 +15,8 @@ import (
 type FastHttpProxy struct {
 	SchemaProvider     proxy.SchemaProvider
 	Host               string
-	Invoker            *middleware.Invoker
+	InvokerPool        *middleware.InvokerPool
+	UserValuePool      sync.Pool
 	invokeMux          sync.Mutex
 	Client             http.Client
 	HandleError        func(err error, w http.ResponseWriter)
@@ -24,69 +25,67 @@ type FastHttpProxy struct {
 	HostClient         *fasthttp.HostClient
 }
 
-func (f *FastHttpProxy) RewriteQuery(ctx context.Context, uri string, query *[]byte, out io.Writer) error {
+func (f *FastHttpProxy) RewriteQuery(userValues map[string][]byte, requestURI []byte, query []byte, out io.Writer) error {
 
-	schema := f.SchemaProvider.GetSchema(uri)
+	schema := f.SchemaProvider.GetSchema(requestURI)
 
-	f.invokeMux.Lock()
+	idx, invoker := f.InvokerPool.Get()
+	defer f.InvokerPool.Free(idx)
 
-	err := f.Invoker.SetSchema(schema)
+	err := invoker.SetSchema(schema)
 	if err != nil {
-		f.invokeMux.Unlock()
 		return err
 	}
 
-	err = f.Invoker.InvokeMiddleWares(ctx, query)
+	err = invoker.InvokeMiddleWares(userValues, query)
 	if err != nil {
-		f.invokeMux.Unlock()
 		return err
 	}
 
-	err = f.Invoker.RewriteRequest(out)
+	err = invoker.RewriteRequest(out)
 	if err != nil {
-		f.invokeMux.Unlock()
 		return err
 	}
 
-	f.invokeMux.Unlock()
 	return err
 }
 
 func (f *FastHttpProxy) HandleRequest(ctx *fasthttp.RequestCtx) {
 
-	c := context.WithValue(ctx, "user", ctx.Request.Header.Peek("user"))
+	userValues := f.UserValuePool.Get().(map[string][]byte)
+	f.CleanUserValues(&userValues)
+	defer f.UserValuePool.Put(userValues)
+
+	userValues["user"] = ctx.Request.Header.Peek("user")
 
 	body := ctx.Request.Body()
 
-	var graphqlJsonRequest GraphqlJsonRequest
-	err := json.Unmarshal(body, &graphqlJsonRequest)
-	if err != nil {
-		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
-		return
+	result := gjson.GetBytes(body, "query")
+	var query []byte
+	if result.Index > 0 {
+		query = body[result.Index : result.Index+len(result.Raw)]
+	} else {
+		body = []byte(result.Raw)
 	}
-
-	query := []byte(graphqlJsonRequest.Query)
 
 	buff := f.BufferPool.Get().(*bytes.Buffer)
 	defer f.BufferPool.Put(buff)
 
-	err = f.RewriteQuery(c, ctx.URI().String(), &query, buff)
+	err := f.RewriteQuery(userValues, ctx.RequestURI(), query, buff)
 	if err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 		return
 	}
 
-	req := GraphqlJsonRequest{
-		Query: buff.String(),
-	}
+	body = body[:0]
 
-	ctx.Request.ResetBody()
-
-	err = json.NewEncoder(ctx.Request.BodyWriter()).Encode(req)
+	body, err = sjson.SetBytes(body, "query", buff.Bytes())
 	if err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 		return
 	}
+
+	ctx.Request.SetBody(body)
 
 	err = f.HostClient.Do(&ctx.Request, &ctx.Response)
 	if err != nil {
@@ -95,4 +94,10 @@ func (f *FastHttpProxy) HandleRequest(ctx *fasthttp.RequestCtx) {
 	}
 
 	// todo: implement the OnResponse handlers / do something with the response
+}
+
+func (f *FastHttpProxy) CleanUserValues(values *map[string][]byte) {
+	for key := range *values {
+		delete(*values, key)
+	}
 }
