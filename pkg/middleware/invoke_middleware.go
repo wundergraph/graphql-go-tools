a9c6eb12367970173d93885c2af4cac578e129b2
Jens Neuse <jens.neuse@gmx.de>
2019-03-24 17:36:09 +0100

    make InvokeMiddleware return errors

--- pkg/middleware/invoke_middleware.go
+++ pkg/middleware/invoke_middleware.go
@@ -10,13 +10,13 @@ import (
 // InvokeMiddleware is a one off middleware invocation helper
 // This should only be used for testing as it's a waste of resources
 // It makes use of panics to don't use this in production!
-func InvokeMiddleware(middleware GraphqlMiddleware, userValues map[string][]byte, schema, request string) string {
+func InvokeMiddleware(middleware GraphqlMiddleware, userValues map[string][]byte, schema, request string) (result string, err error) {
 	parse := parser.NewParser()
-	if err := parse.ParseTypeSystemDefinition([]byte(schema)); err != nil {
-		panic(err)
+	if err = parse.ParseTypeSystemDefinition([]byte(schema)); err != nil {
+		return
 	}
-	if err := parse.ParseExecutableDefinition([]byte(request)); err != nil {
-		panic(err)
+	if err = parse.ParseExecutableDefinition([]byte(request)); err != nil {
+		return
 	}
 	astPrint := printer.New()
 	look := lookup.New(parse, 512)
@@ -24,8 +24,8 @@ func InvokeMiddleware(middleware GraphqlMiddleware, userValues map[string][]byte
 	mod := parser.NewManualAstMod(parse)
 	walk.SetLookup(look)
 
-	if err := middleware.OnRequest(userValues, look, walk, parse, mod); err != nil {
-		panic(err)
+	if err = middleware.OnRequest(userValues, look, walk, parse, mod); err != nil {
+		return
 	}
 
 	walk.SetLookup(look)
@@ -33,8 +33,10 @@ func InvokeMiddleware(middleware GraphqlMiddleware, userValues map[string][]byte
 
 	astPrint.SetInput(parse, look, walk)
 	buff := bytes.Buffer{}
-	if err := astPrint.PrintExecutableSchema(&buff); err != nil {
-		panic(err)
+	if err = astPrint.PrintExecutableSchema(&buff); err != nil {
+		return
 	}
-	return buff.String()
+
+	result = buff.String()
+	return
 }
