2931ee9fbeaedbd33239e3e56d966dc381660222
Jens Neuse <jens.neuse@gmx.de>
2019-03-30 10:30:56 +0100

    refactor arguments definition parsing to use InputValuesDefinitions struct

--- pkg/parser/argumentsdefinition_parser.go
+++ pkg/parser/argumentsdefinition_parser.go
@@ -5,7 +5,7 @@ import (
 	"github.com/jensneuse/graphql-go-tools/pkg/lexing/keyword"
 )
 
-func (p *Parser) parseArgumentsDefinition(index *int) error {
+func (p *Parser) parseArgumentsDefinition(index *int) (err error) {
 
 	start, matches := p.peekExpectSwallow(keyword.BRACKETOPEN)
 	if !matches {
@@ -13,12 +13,11 @@ func (p *Parser) parseArgumentsDefinition(index *int) error {
 	}
 
 	var definition document.ArgumentsDefinition
-	p.initArgumentsDefinition(&definition)
 	definition.Position.MergeStartIntoStart(start.TextPosition)
 
-	err := p.parseInputValueDefinitions(&definition.InputValueDefinitions, keyword.BRACKETCLOSE)
+	definition.InputValueDefinitions, err = p.parseInputValueDefinitions(keyword.BRACKETCLOSE)
 	if err != nil {
-		return err
+		return
 	}
 
 	end, err := p.readExpect(keyword.BRACKETCLOSE, "parseArgumentsDefinition")
