89fb8b36979cdbd572afedad34e8394a20656c85
Jens Neuse <jens.neuse@gmx.de>
2019-04-01 20:58:14 +0200

    add more tests including escaped quotes inside the string

--- pkg/middleware/context_middleware_test.go
+++ pkg/middleware/context_middleware_test.go
@@ -24,22 +24,6 @@ func TestContextMiddleware(t *testing.T) {
 			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
 		}
 	})
-	t.Run("simple string", func(t *testing.T) {
-
-		// it's important to quote the value so the lexer will recognize it's a string value
-		// we might push this including checks into the implementation
-		ctx := context.WithValue(context.Background(), "user", "jsmith@example.org")
-
-		got, err := InvokeMiddleware(&ContextMiddleware{}, ctx, publicSchema, publicQuery)
-		if err != nil {
-			t.Fatal(err)
-		}
-		want := testhelper.UglifyRequestString(privateQuery)
-
-		if want != got {
-			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
-		}
-	})
 	t.Run("unquoted byte slice string should be quoted automatically", func(t *testing.T) {
 
 		// it's important to quote the value so the lexer will recognize it's a string value
@@ -67,6 +51,70 @@ func TestContextMiddleware(t *testing.T) {
 			t.Fatal(err)
 		}
 		want := testhelper.UglifyRequestString(privateQuery)
+
+		if want != got {
+			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
+		}
+	})
+	t.Run("unquoted string should be quoted automatically", func(t *testing.T) {
+
+		// it's important to quote the value so the lexer will recognize it's a string value
+		// we might push this including checks into the implementation
+		ctx := context.WithValue(context.Background(), "user", "jsmith@example.org")
+
+		got, err := InvokeMiddleware(&ContextMiddleware{}, ctx, publicSchema, publicQuery)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := testhelper.UglifyRequestString(privateQuery)
+
+		if want != got {
+			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
+		}
+	})
+	t.Run("left side quoted string should be quoted automatically", func(t *testing.T) {
+
+		// it's important to quote the value so the lexer will recognize it's a string value
+		// we might push this including checks into the implementation
+		ctx := context.WithValue(context.Background(), "user", "\"jsmith@example.org")
+
+		got, err := InvokeMiddleware(&ContextMiddleware{}, ctx, publicSchema, publicQuery)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := testhelper.UglifyRequestString(privateQuery)
+
+		if want != got {
+			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
+		}
+	})
+	t.Run("right side quoted string should be quoted automatically", func(t *testing.T) {
+
+		// it's important to quote the value so the lexer will recognize it's a string value
+		// we might push this including checks into the implementation
+		ctx := context.WithValue(context.Background(), "user", "jsmith@example.org\"")
+
+		got, err := InvokeMiddleware(&ContextMiddleware{}, ctx, publicSchema, publicQuery)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := testhelper.UglifyRequestString(privateQuery)
+
+		if want != got {
+			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
+		}
+	})
+	t.Run("escaped quote inside", func(t *testing.T) {
+
+		// it's important to quote the value so the lexer will recognize it's a string value
+		// we might push this including checks into the implementation
+		ctx := context.WithValue(context.Background(), "user", "jsmith\\\"@example.org")
+
+		got, err := InvokeMiddleware(&ContextMiddleware{}, ctx, publicSchema, publicQuery)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := testhelper.UglifyRequestString(privateQueryWithEscapedQuote)
 
 		if want != got {
 			panic(fmt.Errorf("\nwant:\n%s\ngot:\n%s", want, got))
@@ -125,6 +173,14 @@ query myDocuments {
 const privateQuery = `
 query myDocuments {
 	documents(user: "jsmith@example.org") {
+		sensitiveInformation
+	}
+}
+`
+
+const privateQueryWithEscapedQuote = `
+query myDocuments {
+	documents(user: "jsmith\"@example.org") {
 		sensitiveInformation
 	}
 }
