05bff2a84950655f0263768743f8102e5135bb1c
Jens Neuse <jens.neuse@gmx.de>
2019-04-07 16:16:29 +0200

    implement PrepareSchema for the asset middleware

--- hack/middleware/asset_middleware.go
+++ hack/middleware/asset_middleware.go
@@ -17,6 +17,10 @@ import (
 type AssetUrlMiddleware struct {
 }
 
+func (a *AssetUrlMiddleware) PrepareSchema(ctx context.Context, l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error {
+	return nil
+}
+
 func (a *AssetUrlMiddleware) OnRequest(ctx context.Context, l *lookup.Lookup, w *lookup.Walker, parser *parser.Parser, mod *parser.ManualAstMod) error {
 
 	w.SetLookup(l)
