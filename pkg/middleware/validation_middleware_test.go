004c9ab5188dedcc63eb407519700f28969f9ac9
Jens Neuse <jens.neuse@gmx.de>
2019-04-07 16:18:22 +0200

    add seperate test data for testing the validation middleware

--- pkg/middleware/validation_middleware_test.go
+++ pkg/middleware/validation_middleware_test.go
@@ -7,16 +7,31 @@ import (
 func TestValidationMiddleware(t *testing.T) {
 	t.Run("valid", func(t *testing.T) {
 		query := `query myDocuments {documents {sensitiveInformation}}`
-		_, err := InvokeMiddleware(&ValidationMiddleware{}, nil, publicSchema, query)
+		_, err := InvokeMiddleware(&ValidationMiddleware{}, nil, validationMiddlewarePublicSchema, query)
 		if err != nil {
 			t.Fatal(err)
 		}
 	})
 	t.Run("invalid", func(t *testing.T) {
 		query := `query myDocuments {documents {fieldNotExists}}`
-		_, err := InvokeMiddleware(&ValidationMiddleware{}, nil, publicSchema, query)
+		_, err := InvokeMiddleware(&ValidationMiddleware{}, nil, validationMiddlewarePublicSchema, query)
 		if err == nil {
 			t.Fatal("want err")
 		}
 	})
 }
+
+const validationMiddlewarePublicSchema = `
+schema {
+	query: Query
+}
+
+type Query {
+	documents: [Document]
+}
+
+type Document implements Node {
+	owner: String
+	sensitiveInformation: String
+}
+`
