8bb2c834d39b8fbd6b6ad5ab4a642f8d38b899cb
Jens Neuse <jens.neuse@gmx.de>
2019-06-18 09:16:42 +0200

    WIP add package ast

---
+++ pkg/ast/ast.go
@@ -0,0 +1,233 @@
+package ast
+
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/input"
+	"github.com/jensneuse/graphql-go-tools/pkg/lexing/position"
+)
+
+type DefinitionKind int
+type OperationType int
+type ValueKind int
+type TypeKind int
+
+const (
+	DefinitionKindUnknown DefinitionKind = iota
+	SchemaDefinitionKind
+
+	OperationTypeUndefined OperationType = iota
+	OperationTypeQuery
+	OperationTypeMutation
+	OperationTypeSubscription
+
+	ValueKindUnknown ValueKind = iota
+	ValueKindString
+
+	TypeKindUnknown TypeKind = iota
+	TypeKindNamed
+	TypeKindList
+	TypeKindNonNull
+)
+
+type Document struct {
+	Definitions                  []Definition
+	SchemaDefinitions            []SchemaDefinition
+	RootOperationTypeDefinitions []RootOperationTypeDefinition
+	Directives                   []Directive
+	Arguments                    []Argument
+	ObjectTypeDefinitions        []ObjectTypeDefinition
+	FieldDefinitions             []FieldDefinition
+	NamedTypes                   []NamedType
+}
+
+func NewDocument() *Document {
+	return &Document{
+		Definitions:                  make([]Definition, 48),
+		RootOperationTypeDefinitions: make([]RootOperationTypeDefinition, 3),
+		SchemaDefinitions:            make([]SchemaDefinition, 2),
+		Directives:                   make([]Directive, 16),
+		Arguments:                    make([]Argument, 48),
+		ObjectTypeDefinitions:        make([]ObjectTypeDefinition, 24),
+		NamedTypes:                   make([]NamedType, 48),
+	}
+}
+
+func (d *Document) Reset() {
+	d.Definitions = d.Definitions[:0]
+	d.SchemaDefinitions = d.SchemaDefinitions[:0]
+	d.RootOperationTypeDefinitions = d.RootOperationTypeDefinitions[:0]
+	d.Directives = d.Directives[:0]
+	d.Arguments = d.Arguments[:0]
+	d.ObjectTypeDefinitions = d.ObjectTypeDefinitions[:0]
+	d.NamedTypes = d.NamedTypes[:0]
+}
+
+func (d *Document) GetField(ref int) (node FieldDefinition, nextRef int) {
+	node = d.FieldDefinitions[ref]
+	nextRef = node.Next()
+	return
+}
+
+func (d *Document) GetFieldDefinition(ref int) (node FieldDefinition, nextRef int) {
+	node = d.FieldDefinitions[ref]
+	nextRef = node.Next()
+	return
+}
+
+func (d *Document) GetArgument(ref int) (node Argument, nextRef int) {
+	node = d.Arguments[ref]
+	nextRef = node.Next()
+	return
+}
+
+func (d *Document) GetDirective(ref int) (node Directive, nextRef int) {
+	node = d.Directives[ref]
+	nextRef = node.Next()
+	return
+}
+
+func (d *Document) GetRootOperationTypeDefinition(ref int) (node RootOperationTypeDefinition, nextRef int) {
+	node = d.RootOperationTypeDefinitions[ref]
+	nextRef = node.Next()
+	return
+}
+
+func (d *Document) PutRootOperationTypeDefinition(def RootOperationTypeDefinition) int {
+	d.RootOperationTypeDefinitions = append(d.RootOperationTypeDefinitions, def)
+	return len(d.RootOperationTypeDefinitions) - 1
+}
+
+func (d *Document) PutSchemaDefinition(def SchemaDefinition) int {
+	d.SchemaDefinitions = append(d.SchemaDefinitions, def)
+	return len(d.SchemaDefinitions) - 1
+}
+
+func (d *Document) PutDefinition(def Definition) int {
+	d.Definitions = append(d.Definitions, def)
+	return len(d.Definitions) - 1
+}
+
+func (d *Document) PutDirective(directive Directive) int {
+	d.Directives = append(d.Directives, directive)
+	return len(d.Directives) - 1
+}
+
+func (d *Document) PutArgument(argument Argument) int {
+	d.Arguments = append(d.Arguments, argument)
+	return len(d.Arguments) - 1
+}
+
+type Definition struct {
+	Kind DefinitionKind
+	Ref  int
+}
+
+type SchemaDefinition struct {
+	SchemaLiteral                position.Position
+	Directives                   DirectiveList
+	RootOperationTypeDefinitions RootOperationTypeDefinitionList
+}
+
+type iterable struct {
+	next    int
+	hasNext bool
+}
+
+func (i *iterable) SetNext(next int) {
+	if next == -1 {
+		return
+	}
+	i.hasNext = true
+	i.next = next
+}
+
+func (i iterable) Next() int {
+	if i.hasNext {
+		return i.next
+	}
+	return -1
+}
+
+type RootOperationTypeDefinition struct {
+	iterable
+	OperationType OperationType     // one of query, mutation, subscription
+	Colon         position.Position // :
+	NamedType     NamedType         // e.g. Query
+}
+
+type Directive struct {
+	iterable
+	At           position.Position        // @
+	Name         input.ByteSliceReference // e.g. include
+	ArgumentList ArgumentList             // e.g. (if: true)
+}
+
+type FieldDefinition struct {
+	iterable
+	Description         Description              // optional e.g. "FieldDefinition is ..."
+	Name                input.ByteSliceReference // e.g. foo
+	ArgumentsDefinition InputValueDefinitionList // optional
+	Colon               position.Position        // :
+	Type                Type                     // e.g. String
+	Directives          DirectiveList            // e.g. @foo
+}
+
+type NamedType struct {
+	Name input.ByteSliceReference // e.g. String
+}
+
+type ListType struct {
+	Open  position.Position // [
+	Type  Type              // e.g. String or String !
+	Close position.Position // ]
+}
+
+type NonNullType struct {
+	Type Type              // e.g. String
+	Bang position.Position // !
+}
+
+type Argument struct {
+	iterable
+	Name  input.ByteSliceReference // e.g. foo
+	Colon position.Position        // :
+	Value Value                    // e.g. 100 or "Bar"
+}
+
+type Value struct {
+	Kind ValueKind                // e.g. 100 or "Bar"
+	Raw  input.ByteSliceReference // raw byte reference to content
+}
+
+type Description struct {
+	IsDefined bool
+	Body      input.ByteSliceReference // literal
+	Position  position.Position        // " (single line) or """ multi line
+}
+
+type ObjectTypeDefinition struct {
+	Description          Description              // optional, e.g. "type Foo is ..."
+	TypeLiteral          position.Position        // type
+	Name                 input.ByteSliceReference // e.g. Foo
+	ImplementsInterfaces NamedTypeList            // e.g implements Bar & Baz
+	Directives           DirectiveList            // e.g. @foo
+	FieldsDefinition     FieldDefinitionList      // { foo:Bar bar(baz:String) }
+}
+
+type InputValueDefinition struct {
+	Description  Description              // optional, e.g. "input Foo is..."
+	Name         input.ByteSliceReference // e.g. Foo
+	Colon        position.Position        // :
+	Type         Type                     // e.g. String
+	DefaultValue                          // e.g. = "Bar"
+	Directives   DirectiveList            // e.g. @baz
+}
+
+type Type struct {
+	TypeKind  TypeKind // one of Named,List,NonNull
+	Reference int      // reference to the actual type, depending on kind
+}
+
+type DefaultValue struct {
+	Equals position.Position // =
+	Value  Value             // e.g. "Foo"
+}
---
+++ pkg/ast/list.go
@@ -0,0 +1,50 @@
+//go:generate genny -in=$GOFILE -out=gen-$GOFILE gen "Node=Directive,FieldDefinition,RootOperationTypeDefinition,Argument,NamedType,InputValueDefinition"
+package ast
+
+import (
+	"github.com/cheekybits/genny/generic"
+	"github.com/jensneuse/graphql-go-tools/pkg/lexing/position"
+)
+
+type Node generic.Type
+
+type NodeList struct {
+	Open          position.Position
+	Close         position.Position
+	current       Node
+	currentRef    int
+	nextRef       int
+	isInitialized bool
+}
+
+type NodeGetter interface {
+	GetNode(ref int) (node Node, nextRef int)
+}
+
+func NewNodeList(first int) NodeList {
+	nodeList := NodeList{}
+	nodeList.SetFirst(first)
+	return nodeList
+}
+
+func (n *NodeList) SetFirst(first int) {
+	n.nextRef = first
+	n.isInitialized = first != -1
+}
+
+func (n *NodeList) HasNext() bool {
+	return n.isInitialized && n.nextRef != -1
+}
+
+func (n *NodeList) Next(getter NodeGetter) bool {
+	if !n.isInitialized || n.nextRef == -1 {
+		return false
+	}
+	n.currentRef = n.nextRef
+	n.current, n.nextRef = getter.GetNode(n.nextRef)
+	return true
+}
+
+func (n *NodeList) Value() (Node, int) {
+	return n.current, n.currentRef
+}
