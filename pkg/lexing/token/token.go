8dd7a47f6f61df18560149aba767c2e06a165a28
Jens Neuse <jens.neuse@gmx.de>
2019-08-19 09:24:24 +0200

    update input package name

--- pkg/lexing/token/token.go
+++ pkg/lexing/token/token.go
@@ -2,14 +2,14 @@ package token
 
 import (
 	"fmt"
-	"github.com/jensneuse/graphql-go-tools/pkg/input"
+	"github.com/jensneuse/graphql-go-tools/pkg/ast"
 	"github.com/jensneuse/graphql-go-tools/pkg/lexing/keyword"
 	"github.com/jensneuse/graphql-go-tools/pkg/lexing/position"
 )
 
 type Token struct {
 	Keyword      keyword.Keyword
-	Literal      input.ByteSliceReference
+	Literal      ast.ByteSliceReference
 	TextPosition position.Position
 }
 
