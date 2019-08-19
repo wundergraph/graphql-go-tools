81e3c11e25e537a74d8b51bbc2299bd3f9f5869a
Jens Neuse <jens.neuse@gmx.de>
2019-08-19 09:25:01 +0200

    add index to ast

---
+++ pkg/ast/index.go
@@ -0,0 +1,20 @@
+package ast
+
+type Key []byte
+
+// Index is a struct to easily look up objects in a document, e.g. find Nodes (type/interface/union definitions) by name
+type Index struct {
+	QueryTypeName        ByteSlice
+	MutationTypeName     ByteSlice
+	SubscriptionTypeName ByteSlice
+	Nodes                map[string]Node
+}
+
+func (i *Index) Reset() {
+	i.QueryTypeName = i.QueryTypeName[:0]
+	i.MutationTypeName = i.MutationTypeName[:0]
+	i.SubscriptionTypeName = i.SubscriptionTypeName[:0]
+	for j := range i.Nodes {
+		delete(i.Nodes, j)
+	}
+}
