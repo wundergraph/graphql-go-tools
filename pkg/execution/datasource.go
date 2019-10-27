7320421c8ff463ed208445d53e756a1b30955a7b
Jens Neuse <jensneuse@gmx.de>
2019-10-27 15:43:06 +0100

    add Initialize to DataSourcePlanner

--- pkg/execution/datasource.go
+++ pkg/execution/datasource.go
@@ -1,6 +1,9 @@
 package execution
 
-import "github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/ast"
+	"github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
+)
 
 type DataSource interface {
 	Resolve(ctx Context, args ResolvedArgs) []byte
@@ -9,6 +12,7 @@ type DataSource interface {
 type DataSourcePlanner interface {
 	DirectiveName() []byte
 	Plan() (DataSource, []Argument)
+	Initialize(walker *astvisitor.Walker, operation, definition *ast.Document, args []Argument, resolverParameters []ResolverParameter)
 	astvisitor.EnterInlineFragmentVisitor
 	astvisitor.LeaveInlineFragmentVisitor
 	astvisitor.EnterSelectionSetVisitor
