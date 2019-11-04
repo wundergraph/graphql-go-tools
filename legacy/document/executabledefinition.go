aeb644024bf3ccb09e4fccaee0caca23e2cbe3f1
Jens Neuse <jensneuse@gmx.de>
2019-11-04 11:19:25 +0100

    remove legacy code

--- legacy/document/executabledefinition.go
+++
@@ -1,8 +0,0 @@
-package document
-
-// ExecutableDefinition as specified in:
-// http://facebook.github.io/graphql/draft/#ExecutableDefinition
-type ExecutableDefinition struct {
-	OperationDefinitions []int
-	FragmentDefinitions  []int
-}
--- legacy/document/implementsinterfaces.go
+++
@@ -1,5 +0,0 @@
-package document
-
-// ImplementsInterfaces as specified in:
-// http://facebook.github.io/graphql/draft/#ImplementsInterfaces
-type ImplementsInterfaces ByteSliceReferences
