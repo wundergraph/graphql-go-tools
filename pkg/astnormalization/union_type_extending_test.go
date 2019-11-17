e1eb5a8062d98a4a36744d689ee3c235893b43f9
Java-Jonas <jonas.bgerner96@googlemail.com>
2019-11-17 11:51:16 +0100

    feat: improve union type extending tests

--- pkg/astnormalization/union_type_extending_test.go
+++ pkg/astnormalization/union_type_extending_test.go
@@ -3,13 +3,31 @@ package astnormalization
 import "testing"
 
 func TestExtendUnionType(t *testing.T) {
-	t.Run("extend simple union type by UnionMemberType", func(t *testing.T) {
+	t.Run("extend simple union type by directive", func(t *testing.T) {
 		run(extendUnionTypeDefinition, testDefinition, `
 					union Mammal
 					extend union Mammal @deprecated(reason: "some reason")
 					 `, `
 					union Mammal @deprecated(reason: "some reason")
 					extend union Mammal @deprecated(reason: "some reason")
+					`)
+	})
+	t.Run("extend simple union type by UnionMemberType", func(t *testing.T) {
+		run(extendUnionTypeDefinition, testDefinition, `
+					union Mammal
+					extend union Mammal = Cat
+					 `, `
+					union Mammal = Cat
+					extend union Mammal = Cat
+					`)
+	})
+	t.Run("extend union by multiple directives and union members", func(t *testing.T) {
+		run(extendUnionTypeDefinition, testDefinition, `
+					union Mammal
+					extend union Mammal @deprecated(reason: "some reason") @skip(if: false) = Cat | Dog
+					 `, `
+					union Mammal @deprecated(reason: "some reason") @skip(if: false) = Cat | Dog
+					extend union Mammal @deprecated(reason: "some reason") @skip(if: false) = Cat | Dog
 					`)
 	})
 }
