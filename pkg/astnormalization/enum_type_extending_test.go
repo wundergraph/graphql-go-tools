149bd3eca5e493975265d35c12ee707e7328cf29
Java-Jonas <jonas.bgerner96@googlemail.com>
2019-11-17 12:19:39 +0100

    feat: improve enum type extending tests

--- pkg/astnormalization/enum_type_extending_test.go
+++ pkg/astnormalization/enum_type_extending_test.go
@@ -3,13 +3,31 @@ package astnormalization
 import "testing"
 
 func TestExtendEnumType(t *testing.T) {
-	t.Run("extend simple union type by directive", func(t *testing.T) {
+	t.Run("extend simple enum type by directive", func(t *testing.T) {
 		run(extendEnumTypeDefinition, testDefinition, `
 					enum Countries {DE ES NL}
 					extend enum Countries @deprecated(reason: "some reason")
 					 `, `
 					enum Countries @deprecated(reason: "some reason") {DE ES NL} 
 					extend enum Countries @deprecated(reason: "some reason")
+					`)
+	})
+	t.Run("extend simple enum type by enum values", func(t *testing.T) {
+		run(extendEnumTypeDefinition, testDefinition, `
+					enum Countries {DE ES NL}
+					extend enum Countries {EN}
+					 `, `
+					enum Countries {DE ES NL EN}
+					extend enum Countries {EN}
+					`)
+	})
+	t.Run("extend enum type by numtiple enum values and directives", func(t *testing.T) {
+		run(extendEnumTypeDefinition, testDefinition, `
+					enum Countries {DE ES NL}
+					extend enum Countries @deprecated(reason: "some reason") @skip(if: false) {EN IT}
+					 `, `
+					enum Countries @deprecated(reason: "some reason") @skip(if: false) {DE ES NL EN IT}
+					extend enum Countries @deprecated(reason: "some reason") @skip(if: false) {EN IT}
 					`)
 	})
 }
