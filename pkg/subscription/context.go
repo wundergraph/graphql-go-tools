60ad48e134772a43c81c2895a15aa1c884e679ca
Patric Vormstein <pvormstein@googlemail.com>
2020-03-03 11:47:44 +0100

    add context map for subscription cancellations

---
+++ pkg/subscription/context.go
@@ -0,0 +1,23 @@
+package subscription
+
+import (
+	"context"
+)
+
+type subscriptionCancellations map[string]context.CancelFunc
+
+func (sc subscriptionCancellations) Add(id string) context.Context {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	sc[id] = cancelFunc
+	return ctx
+}
+
+func (sc subscriptionCancellations) Cancel(id string) (ok bool) {
+	cancelFunc, ok := sc[id]
+	if !ok {
+		return false
+	}
+
+	cancelFunc()
+	return true
+}
