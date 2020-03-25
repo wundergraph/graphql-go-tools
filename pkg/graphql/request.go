0eac3476b44b2b36c8a53b3d59bc5adf969d2d27
Patric Vormstein <pvormstein@googlemail.com>
2020-03-25 10:36:26 +0100

    implement request parsing from reader

--- pkg/graphql/request.go
+++ pkg/graphql/request.go
@@ -2,8 +2,12 @@ package graphql
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
+	"io/ioutil"
 )
+
+var ErrEmptyRequest = errors.New("the provided request is empty")
 
 type Request struct {
 	OperationName string          `json:"operation_name"`
@@ -12,7 +16,26 @@ type Request struct {
 }
 
 func UnmarshalRequest(reader io.Reader) (*Request, error) {
-	return &Request{}, nil
+	requestBytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(requestBytes) == 0 {
+		return nil, ErrEmptyRequest
+	}
+
+	var request Request
+	err = json.Unmarshal(requestBytes, &request)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(request.Query) == 0 {
+		return nil, ErrEmptyRequest
+	}
+
+	return &request, nil
 }
 
 func (r Request) ValidateForSchema(schema *Schema) (valid bool, errors OperationValidationErrors) {
---
+++ pkg/graphql/request_test.go
@@ -0,0 +1,43 @@
+package graphql
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnmarshalRequest(t *testing.T) {
+	t.Run("should return error when request is empty", func(t *testing.T) {
+		requestBytes := []byte("")
+		requestBuffer := bytes.NewBuffer(requestBytes)
+
+		request, err := UnmarshalRequest(requestBuffer)
+
+		assert.Error(t, err)
+		assert.Equal(t, ErrEmptyRequest, err)
+		assert.Nil(t, request)
+	})
+
+	t.Run("should return error when query is empty", func(t *testing.T) {
+		requestBytes := []byte("{\"query\": \"\"}")
+		requestBuffer := bytes.NewBuffer(requestBytes)
+
+		request, err := UnmarshalRequest(requestBuffer)
+
+		assert.Error(t, err)
+		assert.Equal(t, ErrEmptyRequest, err)
+		assert.Nil(t, request)
+	})
+
+	t.Run("should successfully unmarshal request", func(t *testing.T) {
+		requestBytes := []byte("{\"operation_name\": \"Hello\", \"variables\": \"\", \"query\": \"query Hello { hello }\"}")
+		requestBuffer := bytes.NewBuffer(requestBytes)
+
+		request, err := UnmarshalRequest(requestBuffer)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello", request.OperationName)
+		assert.Equal(t, "query Hello { hello }", request.Query)
+	})
+}
