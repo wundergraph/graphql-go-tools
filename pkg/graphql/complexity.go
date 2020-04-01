e5fe16ed841b2c2fa90735816461976c30d7dfc9
Sergey Petrunin <sergey@tyk.io>
2020-04-01 21:15:31 +0300

    implement initial default complexity calculator

--- pkg/graphql/complexity.go
+++ pkg/graphql/complexity.go
@@ -1,14 +1,27 @@
 package graphql
 
-var DefaultComplexityCalulator = DefaultComplexityCalculator{}
+import (
+	"github.com/jensneuse/graphql-go-tools/pkg/ast"
+	"github.com/jensneuse/graphql-go-tools/pkg/middleware/operation_complexity"
+	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
+)
+
+var DefaultComplexityCalculator = defaultComplexityCalculator{}
 
 type ComplexityCalculator interface {
-	Calculate(schema *Schema, request *Request) int
+	Calculate(operation, definition *ast.Document) (nodeCount, complexity int, err error)
 }
 
-type DefaultComplexityCalculator struct {
+type defaultComplexityCalculator struct {
 }
 
-func (d DefaultComplexityCalculator) Calculate(schema *Schema, request *Request) int {
-	return 1
+func (d defaultComplexityCalculator) Calculate(operation, definition *ast.Document) (nodeCount, complexity int, err error) {
+	report := operationreport.Report{}
+	nodeCount, complexity = operation_complexity.CalculateOperationComplexity(operation, definition, &report)
+
+	if report.HasErrors() {
+		return 0, 0, report
+	}
+
+	return nodeCount, complexity, nil
 }
