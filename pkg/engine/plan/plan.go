7269fca8aa2a61cfe8fe81151f850e427e7f22f5
Jens Neuse <jens.neuse@gmx.de>
2020-04-20 21:03:45 +0200

    initial version of planner

--- pkg/engine/plan/plan.go
+++ pkg/engine/plan/plan.go
@@ -1,6 +1,9 @@
 package plan
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/jensneuse/graphql-go-tools/pkg/ast"
 	"github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/resolve"
@@ -55,9 +58,15 @@ type Planner struct {
 func NewPlanner(definition *ast.Document) *Planner {
 
 	walker := astvisitor.NewWalker(48)
-	visitor := &visitor{}
+	visitor := &visitor{
+		Walker:     &walker,
+		definition: definition,
+	}
 
-	walker.RegisterEnterDocumentVisitor(visitor)
+	walker.RegisterDocumentVisitor(visitor)
+	walker.RegisterOperationDefinitionVisitor(visitor)
+	walker.RegisterSelectionSetVisitor(visitor)
+	walker.RegisterFieldVisitor(visitor)
 
 	return &Planner{
 		definition: definition,
@@ -66,16 +75,117 @@ func NewPlanner(definition *ast.Document) *Planner {
 	}
 }
 
-func (p *Planner) Plan(operation *ast.Document, report *operationreport.Report) (plan Plan,err error) {
-
-	p.walker.Walk(operation, p.definition,report)
-
-	return
+func (p *Planner) Plan(operation *ast.Document, operationName []byte, report *operationreport.Report) (plan Plan) {
+	p.visitor.operation = operation
+	p.visitor.opName = operationName
+	p.walker.Walk(operation, p.definition, report)
+	return p.visitor.plan
 }
 
 type visitor struct {
+	*astvisitor.Walker
+	definition, operation *ast.Document
+	opName                []byte
+	plan                  Plan
+	currentObject         *resolve.Object
+	currentFields         *[]resolve.Field
+	fields                []*[]resolve.Field
 }
 
 func (v *visitor) EnterDocument(operation, definition *ast.Document) {
+
+}
+
+func (v *visitor) LeaveDocument(operation, definition *ast.Document) {
+
+}
+
+func (v *visitor) EnterOperationDefinition(ref int) {
+	if bytes.Equal(v.operation.OperationDefinitionNameBytes(ref), v.opName) {
+		v.currentObject = &resolve.Object{}
+		v.plan = &SynchronousResponsePlan{
+			Response: resolve.GraphQLResponse{
+				Data: v.currentObject,
+			},
+		}
+	} else {
+		v.SkipNode()
+	}
+}
+
+func (v *visitor) LeaveOperationDefinition(ref int) {
+	v.currentObject = nil
+}
+
+func (v *visitor) EnterSelectionSet(ref int) {
+	v.currentObject.FieldSets = append(v.currentObject.FieldSets, resolve.FieldSet{
+		Fields: []resolve.Field{},
+	})
+	v.currentFields = &v.currentObject.FieldSets[len(v.currentObject.FieldSets)-1].Fields
+	v.fields = append(v.fields, v.currentFields)
+}
+
+func (v *visitor) LeaveSelectionSet(ref int) {
+	v.fields = v.fields[:len(v.fields)-1]
+	if len(v.fields) == 0 {
+		return
+	}
+	v.currentFields = v.fields[len(v.fields)-1]
+}
+
+func (v *visitor) EnterField(ref int) {
+	fmt.Println(v.operation.FieldNameString(ref), v.Path.String())
+	fieldName := v.operation.FieldNameBytes(ref)
+	fieldNameStr := v.operation.FieldNameString(ref)
+	definition, ok := v.definition.NodeFieldDefinitionByName(v.EnclosingTypeDefinition, fieldName)
+	if !ok {
+		return
+	}
+	fieldDefinitionType := v.definition.FieldDefinitionType(definition)
+	typeName := v.definition.ResolveTypeNameString(fieldDefinitionType)
+
+	var value resolve.Node
+
+	switch typeName {
+	case "String":
+		value = &resolve.String{
+			Path: []string{fieldNameStr},
+		}
+	case "Boolean":
+		value = &resolve.Boolean{
+			Path: []string{fieldNameStr},
+		}
+	case "Int":
+		value = &resolve.Integer{
+			Path: []string{fieldNameStr},
+		}
+	case "Float":
+		value = &resolve.Float{
+			Path: []string{fieldNameStr},
+		}
+	default:
+		obj := &resolve.Object{}
+		value = obj
+		defer func() {
+			v.currentObject = obj
+		}()
+	}
+
+	isList := v.definition.TypeIsList(fieldDefinitionType)
+	if isList {
+		list := &resolve.Array{
+			Path: []string{fieldNameStr},
+			Item: value,
+		}
+		value = list
+	}
+
+	*v.currentFields = append(*v.currentFields, resolve.Field{
+		Name:  fieldName,
+		Value: value,
+	})
+}
+
+func (v *visitor) LeaveField(ref int) {
 
 }
--- pkg/engine/plan/plan_test.go
+++ pkg/engine/plan/plan_test.go
@@ -1,25 +1,171 @@
 package plan
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
+	"github.com/jensneuse/diffview"
+
 	"github.com/jensneuse/graphql-go-tools/internal/pkg/unsafeparser"
+	"github.com/jensneuse/graphql-go-tools/pkg/astnormalization"
 	"github.com/jensneuse/graphql-go-tools/pkg/asttransform"
+	"github.com/jensneuse/graphql-go-tools/pkg/astvalidation"
+	"github.com/jensneuse/graphql-go-tools/pkg/engine/resolve"
+	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
 )
 
 func TestPlanner_Plan(t *testing.T) {
-	test := func(definition string) func(t *testing.T) {
+	test := func(definition, operation, operationName string, expectedPlan Plan) func(t *testing.T) {
 		return func(t *testing.T) {
 			def := unsafeparser.ParseGraphqlDocumentString(definition)
+			op := unsafeparser.ParseGraphqlDocumentString(operation)
 			err := asttransform.MergeDefinitionWithBaseSchema(&def)
 			if err != nil {
 				t.Fatal(err)
 			}
-			NewPlanner(&def)
+			norm := astnormalization.NewNormalizer(true)
+			var report operationreport.Report
+			norm.NormalizeOperation(&op, &def, &report)
+			valid := astvalidation.DefaultOperationValidator()
+			valid.Validate(&op, &def, &report)
+			p := NewPlanner(&def)
+			plan := p.Plan(&op, []byte(operationName), &report)
+			if report.HasErrors() {
+				t.Fatal(report.Error())
+			}
+			if !reflect.DeepEqual(expectedPlan, plan) {
+				diffview.NewGoland().DiffViewAny("diff", expectedPlan, plan)
+				t.Errorf("want:\n%s\ngot:\n%s\n", spew.Sdump(expectedPlan), spew.Sdump(plan))
+			}
 		}
 	}
 
-	t.Run("empty plan", test(testDefinition))
+	t.Run("simple named Query", test(testDefinition, `
+		query MyQuery($id: ID!){
+			droid(id: $id){
+				name
+				friends {
+					name
+				}
+				primaryFunction
+			}
+		}
+	`, "MyQuery", &SynchronousResponsePlan{
+		Response: resolve.GraphQLResponse{
+			Data: &resolve.Object{
+				FieldSets: []resolve.FieldSet{
+					{
+						Fields: []resolve.Field{
+							{
+								Name: []byte("droid"),
+								Value: &resolve.Object{
+									FieldSets: []resolve.FieldSet{
+										{
+											Fields: []resolve.Field{
+												{
+													Name: []byte("name"),
+													Value: &resolve.String{
+														Path: []string{"name"},
+													},
+												},
+												{
+													Name: []byte("friends"),
+													Value: &resolve.Array{
+														Path: []string{"friends"},
+														Item: &resolve.Object{
+															FieldSets: []resolve.FieldSet{
+																{
+																	Fields: []resolve.Field{
+																		{
+																			Name: []byte("name"),
+																			Value: &resolve.String{
+																				Path: []string{"name"},
+																			},
+																		},
+																	},
+																},
+															},
+														},
+													},
+												},
+												{
+													Name: []byte("primaryFunction"),
+													Value: &resolve.String{
+														Path: []string{"primaryFunction"},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}))
 }
 
-const testDefinition = ``
+const testDefinition = `
+union SearchResult = Human | Droid | Starship
+
+schema {
+    query: Query
+    mutation: Mutation
+    subscription: Subscription
+}
+
+type Query {
+    hero: Character
+    droid(id: ID!): Droid
+    search(name: String!): SearchResult
+}
+
+type Mutation {
+    createReview(episode: Episode!, review: ReviewInput!): Review
+}
+
+type Subscription {
+    remainingJedis: Int!
+}
+
+input ReviewInput {
+    stars: Int!
+    commentary: String
+}
+
+type Review {
+    id: ID!
+    stars: Int!
+    commentary: String
+}
+
+enum Episode {
+    NEWHOPE
+    EMPIRE
+    JEDI
+}
+
+interface Character {
+    name: String!
+    friends: [Character]
+}
+
+type Human implements Character {
+    name: String!
+    height: String!
+    friends: [Character]
+}
+
+type Droid implements Character {
+    name: String!
+    primaryFunction: String!
+    friends: [Character]
+}
+
+type Startship {
+    name: String!
+    length: Float!
+}`
