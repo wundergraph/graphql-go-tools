a71f7578eb2e8008a5a29154c82ebe7fc112c325
Patric Vormstein <pvormstein@googlemail.com>
2021-02-01 15:18:35 +0100

    add function to check for blocked and allowed fields

--- pkg/graphql/request_fields_validator_test.go
+++ pkg/graphql/request_fields_validator_test.go
@@ -42,3 +42,93 @@ func TestFieldsValidator_Validate(t *testing.T) {
 		assert.Equal(t, 0, result.Errors.Count())
 	})
 }
+
+func TestFieldsValidator_ValidateByFieldList(t *testing.T) {
+	schema := starwarsSchema(t)
+	request := requestForQuery(t, starwars.FileSimpleHeroQuery)
+
+	t.Run("block list", func(t *testing.T) {
+		t.Run("should invalidate if blocked fields are used", func(t *testing.T) {
+			blockList := FieldRestrictionList{
+				Kind: BlockList,
+				Types: []Type{
+					{
+						Name:   "Character",
+						Fields: []string{"name"},
+					},
+				},
+			}
+
+			validator := fieldsValidator{}
+			result, err := validator.ValidateByFieldList(&request, schema, blockList)
+			assert.NoError(t, err)
+			assert.False(t, result.Valid)
+			assert.Equal(t, 1, result.Errors.Count())
+		})
+
+		t.Run("should validate if non-blocked fields are used", func(t *testing.T) {
+			blockList := FieldRestrictionList{
+				Kind: BlockList,
+				Types: []Type{
+					{
+						Name:   "Character",
+						Fields: []string{"friends"},
+					},
+				},
+			}
+
+			validator := fieldsValidator{}
+			result, err := validator.ValidateByFieldList(&request, schema, blockList)
+			assert.NoError(t, err)
+			assert.True(t, result.Valid)
+			assert.Equal(t, 0, result.Errors.Count())
+		})
+	})
+
+	t.Run("allow list", func(t *testing.T) {
+		t.Run("should invalidate if a field which is not allowed is used", func(t *testing.T) {
+			blockList := FieldRestrictionList{
+				Kind: AllowList,
+				Types: []Type{
+					{
+						Name:   "Query",
+						Fields: []string{"hero"},
+					},
+					{
+						Name:   "Character",
+						Fields: []string{"friends"},
+					},
+				},
+			}
+
+			validator := fieldsValidator{}
+			result, err := validator.ValidateByFieldList(&request, schema, blockList)
+			assert.NoError(t, err)
+			assert.False(t, result.Valid)
+			assert.Equal(t, 1, result.Errors.Count())
+		})
+
+		t.Run("should validate if all fields are allowed", func(t *testing.T) {
+			blockList := FieldRestrictionList{
+				Kind: AllowList,
+				Types: []Type{
+					{
+						Name:   "Query",
+						Fields: []string{"hero"},
+					},
+					{
+						Name:   "Character",
+						Fields: []string{"name"},
+					},
+				},
+			}
+
+			validator := fieldsValidator{}
+			result, err := validator.ValidateByFieldList(&request, schema, blockList)
+			assert.NoError(t, err)
+			assert.True(t, result.Valid)
+			assert.Equal(t, 0, result.Errors.Count())
+		})
+	})
+
+}
