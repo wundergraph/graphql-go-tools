dbe4fed3438733af994c5d52eb82da47af7e65e8
chedom <domanchukits@gmail.com>
2021-08-20 13:57:18 +0300

    Add WebSocket init function for authorizing connect

--- pkg/subscription/context.go
+++ pkg/subscription/context.go
@@ -6,6 +6,12 @@ import (
 
 type subscriptionCancellations map[string]context.CancelFunc
 
+func (sc subscriptionCancellations) AddWithParent(id string, parent context.Context) context.Context {
+	ctx, cancelFunc := context.WithCancel(parent)
+	sc[id] = cancelFunc
+	return ctx
+}
+
 func (sc subscriptionCancellations) Add(id string) context.Context {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	sc[id] = cancelFunc
