2bf977346062db37c2ade89fd86a3eef4237fa6b
Sergey Petrunin <neyasut@gmail.com>
2022-01-24 11:09:57 +0200

    remove skips

--- pkg/astvalidation/reference/testsgo/VariablesInAllowedPositionRule_test.go
+++ pkg/astvalidation/reference/testsgo/VariablesInAllowedPositionRule_test.go
@@ -5,8 +5,6 @@ import (
 )
 
 func TestVariablesInAllowedPositionRule(t *testing.T) {
-	t.Skip()
-
 	ExpectErrors := func(t *testing.T, queryStr string) ResultCompare {
 		return ExpectValidationErrors(t, VariablesInAllowedPositionRule, queryStr)
 	}
