a3dae41609cdb81edd781956b17a2d9d3d852241
Patric Vormstein <pvormstein@googlemail.com>
2022-02-08 16:10:43 +0100

    add rule to default definition validator

--- pkg/astvalidation/definition_validation.go
+++ pkg/astvalidation/definition_validation.go
@@ -14,6 +14,7 @@ func DefaultDefinitionValidator() *DefinitionValidator {
 		UniqueEnumValueNames(),
 		KnownTypeNames(),
 		RequireDefinedTypesForExtensions(),
+		ImplementTransitiveInterfaces(),
 	)
 }
 
