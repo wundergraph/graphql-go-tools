06e0c8769d86e74cd0f3fc8e5e9ec090ff17371b
David Stutt <david.s@tyk.io>
2022-05-02 13:01:43 +0100

    [TT-5117] Re-added interface tests [changelog] internal: Re-added interface tests; improved test naming.

--- pkg/federation/sdlmerge/remove_duplicate_fielded_value_types_test.go
+++ pkg/federation/sdlmerge/remove_duplicate_fielded_value_types_test.go
@@ -42,7 +42,7 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
-	t.Run("Identical same name inputs are merged into a single input", func(t *testing.T) {
+	t.Run("Groups of identical same name inputs are respectively merged into single inputs", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			input Pokemon {
 				type: Type!
@@ -121,78 +121,38 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			`, FederatingValueTypeErrorMessage("Trainer"))
 	})
 
-	t.Run("Same name empty objects are merged into a single object", func(t *testing.T) {
+	t.Run("Same name empty interfaces are merged into a single interface", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
-			type Trainer {
+			interface Trainer {
 			}
 
-			type Trainer {
+			interface Trainer {
 			}
 			`, `
-			type Trainer {
+			interface Trainer {
 			}
 			`,
 		)
 	})
 
-	t.Run("Identical same name objects are merged into a single object", func(t *testing.T) {
+	t.Run("Identical same name interfaces are merged into a single interface", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int!
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int!
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int!
 			}
 			`, `
-			type Trainer {
-				name: String!
-				age: Int!
-			}
-			`,
-		)
-	})
-
-	t.Run("Identical same name objects are merged into a single object", func(t *testing.T) {
-		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
-			type Pokemon {
-				type: Type!
-				isEvolved: Boolean!
-			}
-
-			type Trainer {
-				name: String!
-				age: Int!
-			}
-
-			type Trainer {
-				name: String!
-				age: Int!
-			}
-
-			type Trainer {
-				name: String!
-				age: Int!
-			}
-
-			type Pokemon {
-				type: Type!
-				isEvolved: Boolean!
-			}
-			`, `
-			type Pokemon {
-				type: Type!
-				isEvolved: Boolean!
-			}
-
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int!
 			}
@@ -200,38 +160,78 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
-	t.Run("Same name objects with different nullability of fields return an error", func(t *testing.T) {
+	t.Run("Groups of identical same name interfaces are respectively merged into single interfaces", func(t *testing.T) {
+		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			interface Pokemon {
+				type: Type!
+				isEvolved: Boolean!
+			}
+
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+
+			interface Pokemon {
+				type: Type!
+				isEvolved: Boolean!
+			}
+			`, `
+			interface Pokemon {
+				type: Type!
+				isEvolved: Boolean!
+			}
+
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+			`,
+		)
+	})
+
+	t.Run("Same name interfaces with different nullability of fields return an error", func(t *testing.T) {
 		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int!
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String
 				age: Int!
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String!
 				age: Int
 			}
 			`, FederatingValueTypeErrorMessage("Trainer"))
 	})
 
-	t.Run("Same name objects with different fields return an error", func(t *testing.T) {
+	t.Run("Same name interfaces with different fields return an error", func(t *testing.T) {
 		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
-			type Trainer {
+			interface Trainer {
 				name: String
 				age: Int
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String
 				age: Int
 			}
 
-			type Trainer {
+			interface Trainer {
 				name: String
 				age: Int
 				badges: Int
@@ -278,7 +278,7 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
-	t.Run("Identical same name objects are merged into a single object", func(t *testing.T) {
+	t.Run("Groups of identical same name objects are respectively merged into single objects", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			type Pokemon {
 				type: Type!
