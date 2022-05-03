4609a436615276d9aa459bacbf505e3c6f489fcc
David Stutt <david.s@tyk.io>
2022-05-03 13:51:17 +0100

    [TT-5117] Corrected comparison of nested field values. [changelog] internal: Field values are deeply compared; added more tests.
    (cherry picked from commit a68d94d657e320ef6127a91ecf80e2b11e169879)

--- pkg/federation/sdlmerge/remove_duplicate_fielded_value_types_test.go
+++ pkg/federation/sdlmerge/remove_duplicate_fielded_value_types_test.go
@@ -42,10 +42,35 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
+	t.Run("Identical same name inputs are merged into a single input regardless of field order", func(t *testing.T) {
+		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			input Trainer {
+				age: Int!
+				name: String!
+			}
+	
+			input Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			input Trainer {
+				name: String!
+				age: Int!
+			}
+			`, `
+			input Trainer {
+				age: Int!
+				name: String!
+			}
+			`,
+		)
+	})
+
 	t.Run("Groups of identical same name inputs are respectively merged into single inputs", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			input Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 	
@@ -65,12 +90,12 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			}
 	
 			input Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 			`, `
 			input Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 	
@@ -121,6 +146,41 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			`, FederatingValueTypeErrorMessage("Trainer"))
 	})
 
+	t.Run("Same name inputs with a slight difference in nested field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			input Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+	
+			input Pokemon {
+				type: [[[[Type!]]]!]!
+			}
+	
+			input Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+			`, FederatingValueTypeErrorMessage("Pokemon"))
+	})
+
+	t.Run("Same name inputs with different non-nullable field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			input Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			input Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			input Trainer {
+				name: String!
+				age: String!
+			}
+			`, FederatingValueTypeErrorMessage("Trainer"))
+	})
+
 	t.Run("Same name empty interfaces are merged into a single interface", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			interface Trainer {
@@ -160,10 +220,35 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
+	t.Run("Identical same name interfaces are merged into a single input regardless of field order", func(t *testing.T) {
+		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			interface Trainer {
+				age: Int!
+				name: String!
+			}
+	
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+			`, `
+			interface Trainer {
+				age: Int!
+				name: String!
+			}
+			`,
+		)
+	})
+
 	t.Run("Groups of identical same name interfaces are respectively merged into single interfaces", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			interface Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 
@@ -183,12 +268,12 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			}
 
 			interface Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 			`, `
 			interface Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 
@@ -239,6 +324,41 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			`, FederatingValueTypeErrorMessage("Trainer"))
 	})
 
+	t.Run("Same name interfaces with a slight difference in nested field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			interface Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+	
+			interface Pokemon {
+				type: [[[[Type!]]]!]!
+			}
+	
+			interface Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+			`, FederatingValueTypeErrorMessage("Pokemon"))
+	})
+
+	t.Run("Same name interfaces with different non-nullable field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			interface Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			interface Trainer {
+				name: String!
+				age: String!
+			}
+			`, FederatingValueTypeErrorMessage("Trainer"))
+	})
+
 	t.Run("Same name empty objects are merged into a single object", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			type Trainer {
@@ -278,10 +398,35 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 		)
 	})
 
+	t.Run("Identical same name objects are merged into a single input regardless of field order", func(t *testing.T) {
+		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			type Trainer {
+				age: Int!
+				name: String!
+			}
+	
+			type Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			type Trainer {
+				name: String!
+				age: Int!
+			}
+			`, `
+			type Trainer {
+				age: Int!
+				name: String!
+			}
+			`,
+		)
+	})
+
 	t.Run("Groups of identical same name objects are respectively merged into single objects", func(t *testing.T) {
 		run(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
 			type Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 
@@ -301,12 +446,12 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 			}
 
 			type Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 			`, `
 			type Pokemon {
-				type: Type!
+				type: [Type!]!
 				isEvolved: Boolean!
 			}
 
@@ -353,6 +498,41 @@ func TestRemoveDuplicateFieldedValueTypes(t *testing.T) {
 				name: String
 				age: Int
 				badges: Int
+			}
+			`, FederatingValueTypeErrorMessage("Trainer"))
+	})
+
+	t.Run("Same name objects with a slight difference in nested field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			type Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+	
+			type Pokemon {
+				type: [[[[Type!]]]!]!
+			}
+	
+			type Pokemon {
+				type: [[[[Type!]]!]!]!
+			}
+			`, FederatingValueTypeErrorMessage("Pokemon"))
+	})
+
+	t.Run("Same name objects with different non-nullable field values return an error", func(t *testing.T) {
+		runAndExpectError(t, newRemoveDuplicateFieldedValueTypesVisitor(), `
+			type Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			type Trainer {
+				name: String!
+				age: Int!
+			}
+	
+			type Trainer {
+				name: String!
+				age: String!
 			}
 			`, FederatingValueTypeErrorMessage("Trainer"))
 	})
