cb7cb23c90c2fc165eb41eb2cd71676ace0f7c87
Burak Sezer <burak.sezer.developer@gmail.com>
2022-08-11 15:31:22 +0300

    [TT-6022] Fix broken Kafka data source tests

--- pkg/engine/datasource/kafka_datasource/kafka_datasource_test.go
+++ pkg/engine/datasource/kafka_datasource/kafka_datasource_test.go
@@ -46,7 +46,7 @@ func runTestOnTestDefinition(operation, operationName string, expectedPlan plan.
 				Custom: ConfigJSON(Configuration{
 					Subscription: SubscriptionConfiguration{
 						BrokerAddresses: []string{"localhost:9092"},
-						Topic:           "test.topic",
+						Topics:          []string{"test.topic"},
 						GroupID:         "test.consumer.group",
 						ClientID:        "test.client.id",
 						KafkaVersion:    testMockKafkaVersion,
@@ -91,7 +91,7 @@ func TestKafkaDataSource(t *testing.T) {
 	`, "RemainingJedis", &plan.SubscriptionResponsePlan{
 		Response: &resolve.GraphQLSubscription{
 			Trigger: resolve.GraphQLSubscriptionTrigger{
-				Input: []byte(fmt.Sprintf(`{"broker_addresses":["localhost:9092"],"topic":"test.topic","group_id":"test.consumer.group","client_id":"test.client.id","kafka_version":"%s","start_consuming_latest":false,"balance_strategy":"%s","isolation_level":"%s","sasl":{"enable":true,"user":"%s","password":"%s"}}`,
+				Input: []byte(fmt.Sprintf(`{"broker_addresses":["localhost:9092"],"topics":["test.topic"],"group_id":"test.consumer.group","client_id":"test.client.id","kafka_version":"%s","start_consuming_latest":false,"balance_strategy":"%s","isolation_level":"%s","sasl":{"enable":true,"user":"%s","password":"%s"}}`,
 					testMockKafkaVersion,
 					DefaultBalanceStrategy,
 					DefaultIsolationLevel,
@@ -133,7 +133,7 @@ func TestKafkaDataSource(t *testing.T) {
 	`, "SubscriptionWithVariables", &plan.SubscriptionResponsePlan{
 		Response: &resolve.GraphQLSubscription{
 			Trigger: resolve.GraphQLSubscriptionTrigger{
-				Input: []byte(fmt.Sprintf(`{"broker_addresses":["localhost:9092"],"topic":"test.topic.$$0$$","group_id":"test.consumer.group","client_id":"test.client.id","kafka_version":"%s","start_consuming_latest":false,"balance_strategy":"%s","isolation_level":"%s","sasl":{"enable":true,"user":"%s","password":"%s"}}`,
+				Input: []byte(fmt.Sprintf(`{"broker_addresses":["localhost:9092"],"topics":["test.topic.$$0$$"],"group_id":"test.consumer.group","client_id":"test.client.id","kafka_version":"%s","start_consuming_latest":false,"balance_strategy":"%s","isolation_level":"%s","sasl":{"enable":true,"user":"%s","password":"%s"}}`,
 					testMockKafkaVersion,
 					DefaultBalanceStrategy,
 					DefaultIsolationLevel,
@@ -180,7 +180,7 @@ func TestKafkaDataSource(t *testing.T) {
 				Custom: ConfigJSON(Configuration{
 					Subscription: SubscriptionConfiguration{
 						BrokerAddresses: []string{"localhost:9092"},
-						Topic:           "test.topic.{{.arguments.bar}}",
+						Topics:          []string{"test.topic.{{.arguments.bar}}"},
 						GroupID:         "test.consumer.group",
 						ClientID:        "test.client.id",
 						KafkaVersion:    testMockKafkaVersion,
