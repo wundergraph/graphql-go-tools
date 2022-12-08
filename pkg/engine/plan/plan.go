wundergraph/graphql-go-tools

commit cfceb5fdb0613cc9d398a2c9aea3f496db54336d

Merge pull request #373
* Merge pull request #258 from TykTechnologies/fix/TT-4816/access-neste…

* fix not removing quotes for string arguments (#263)

pkg/engine/plan/plan.go
@@ -1035,14 +1035,26 @@ func (v *Visitor) resolveInputTemplates(config objectFetchConfiguration, input *
 			if !v.Operation.OperationDefinitionHasVariableDefinition(v.operationDefinition, variableValue) {
 				break // omit optional argument when variable is not defined
 			}
-			variable := &resolve.ContextVariable{
-				Path: []string{variableValue},
-			}
 			variableDefinition, exists := v.Operation.VariableDefinitionByNameAndOperation(v.operationDefinition, v.Operation.VariableValueNameBytes(value.Ref))
 			if !exists {
 				break
 			}
 			variableTypeRef := v.Operation.VariableDefinitions[variableDefinition].Type
+			typeName := v.Operation.ResolveTypeNameBytes(v.Operation.VariableDefinitions[variableDefinition].Type)
+			node, exists := v.Definition.Index.FirstNodeByNameBytes(typeName)
+			if !exists {
+				break
+			}
+			var variablePath []string
+			switch node.Kind {
+			case ast.NodeKindInputObjectTypeDefinition:
+				variablePath = append(variablePath, path...)
+			default:
+				variablePath = append(variablePath, variableValue)
+			}
+			variable := &resolve.ContextVariable{
+				Path: variablePath,
+			}
 
 			if fieldConfig, ok := v.fieldConfigs[config.fieldRef]; ok {
 				if argumentConfig := fieldConfig.Arguments.ForName(argumentName); argumentConfig != nil {
@@ -1050,7 +1062,7 @@ func (v *Visitor) resolveInputTemplates(config objectFetchConfiguration, input *
 					case RenderArgumentAsArrayCSV:
 						variable.Renderer = resolve.NewCSVVariableRendererFromTypeRef(v.Operation, v.Definition, variableTypeRef)
 					case RenderArgumentDefault:
-						renderer, err := resolve.NewPlainVariableRendererWithValidationFromTypeRef(v.Operation, v.Definition, variableTypeRef)
+						renderer, err := resolve.NewPlainVariableRendererWithValidationFromTypeRef(v.Operation, v.Definition, variableTypeRef, variablePath...)
 						if err != nil {
 							break
 						}
@@ -1072,7 +1084,7 @@ func (v *Visitor) resolveInputTemplates(config objectFetchConfiguration, input *
 			}
 
 			if variable.Renderer == nil {
-				renderer, err := resolve.NewPlainVariableRendererWithValidationFromTypeRef(v.Operation, v.Definition, variableTypeRef)
+				renderer, err := resolve.NewPlainVariableRendererWithValidationFromTypeRef(v.Operation, v.Definition, variableTypeRef, variablePath...)
 				if err != nil {
 					break
 				}

pkg/engine/datasource/rest_datasource/rest_datasource_test.go
@@ -33,6 +33,14 @@ const (
 			withArrayArguments(names: [String]): Friend
 		}
 
+		input InputFriend {
+			name: String!
+		}
+
+		type Mutation {
+			createFriend(friend: InputFriend!): Friend
+		}
+
 		type Friend {
 			name: String
 			pet: Pet
@@ -106,6 +114,14 @@ const (
 	arrayArgumentOperation = `
 		query ArgumentQuery {
 			withArrayArguments(names: ["foo","bar"]) {
+				name
+			}
+		}
+	`
+
+	createFriendOperation = `
+		mutation CreateFriend($friendVariable: InputFriend!) {
+			createFriend(friend: $friendVariable) {
 				name
 			}
 		}
@@ -296,6 +312,75 @@ func TestFastHttpJsonDataSourcePlanning(t *testing.T) {
 				{
 					TypeName:              "Query",
 					FieldName:             "withArgument",
+					DisableDefaultMapping: true,
+				},
+			},
+			DisableResolveFieldPositions: true,
+		},
+	))
+	t.Run("mutation with nested argument", datasourcetesting.RunTest(schema, createFriendOperation, "CreateFriend",
+		&plan.SynchronousResponsePlan{
+			Response: &resolve.GraphQLResponse{
+				Data: &resolve.Object{
+					Fetch: &resolve.SingleFetch{
+						BufferId:   0,
+						Input:      `{"body":"{"friend":{"name":"$$0$$"}}","method":"POST","url":"https://example.com/$$0$$"}`,
+						DataSource: &Source{},
+						Variables: resolve.NewVariables(
+							&resolve.ContextVariable{
+								Path:     []string{"friend", "name"},
+								Renderer: resolve.NewPlainVariableRendererWithValidation(`{"type":["string"]}`),
+							},
+						),
+						DisallowSingleFlight: true,
+						DisableDataLoader:    true,
+						DataSourceIdentifier: []byte("rest_datasource.Source"),
+					},
+					Fields: []*resolve.Field{
+						{
+							BufferID:  0,
+							HasBuffer: true,
+							Name:      []byte("createFriend"),
+							Value: &resolve.Object{
+								Nullable: true,
+								Fields: []*resolve.Field{
+									{
+										Name: []byte("name"),
+										Value: &resolve.String{
+											Path:     []string{"name"},
+											Nullable: true,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		plan.Configuration{
+			DataSources: []plan.DataSourceConfiguration{
+				{
+					RootNodes: []plan.TypeField{
+						{
+							TypeName:   "Mutation",
+							FieldNames: []string{"createFriend"},
+						},
+					},
+					Custom: ConfigJSON(Configuration{
+						Fetch: FetchConfiguration{
+							URL:    "https://example.com/{{ .arguments.friend.name }}",
+							Method: "POST",
+							Body:   "{\"friend\":{\"name\":\"{{ .arguments.friend.name }}\"}}",
+						},
+					}),
+					Factory: &Factory{},
+				},
+			},
+			Fields: []plan.FieldConfiguration{
+				{
+					TypeName:              "Mutation",
+					FieldName:             "createFriend",
 					DisableDefaultMapping: true,
 				},
 			},

pkg/engine/resolve/inputtemplate.go
@@ -62,7 +62,7 @@ func (i *InputTemplate) renderObjectVariable(ctx context.Context, variables []by
 	if valueType == jsonparser.String {
 		value = variables[offset-len(value)-2 : offset]
 		switch segment.Renderer.GetKind() {
-		case VariableRendererKindPlain:
+		case VariableRendererKindPlain, VariableRendererKindPlanWithValidation:
 			if plainRenderer, ok := (segment.Renderer).(*PlainVariableRenderer); ok {
 				plainRenderer.rootValueType.Value = valueType
 			}
@@ -79,6 +79,12 @@ func (i *InputTemplate) renderContextVariable(ctx *Context, segment TemplateSegm
 	}
 	if valueType == jsonparser.String {
 		value = ctx.Variables[offset-len(value)-2 : offset]
+		switch segment.Renderer.GetKind() {
+		case VariableRendererKindPlain, VariableRendererKindPlanWithValidation:
+			if plainRenderer, ok := (segment.Renderer).(*PlainVariableRenderer); ok {
+				plainRenderer.rootValueType.Value = valueType
+			}
+		}
 	}
 	return segment.Renderer.RenderVariable(ctx, value, preparedInput)
 }

pkg/engine/resolve/resolve_test.go
@@ -2243,7 +2243,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 									},
 									{
 										SegmentType: StaticSegmentType,
-										Data:        []byte(`,"firstArg":`),
+										Data:        []byte(`,"firstArg":"`),
 									},
 									{
 										SegmentType:        VariableSegmentType,
@@ -2253,7 +2253,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 									},
 									{
 										SegmentType: StaticSegmentType,
-										Data:        []byte(`}}}`),
+										Data:        []byte(`"}}}`),
 									},
 								},
 							},
@@ -2915,7 +2915,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 									InputTemplate: InputTemplate{
 										Segments: []TemplateSegment{
 											{
-												Data:        []byte(`{"method":"POST","url":"http://localhost:4002","body":{"query":"query($representations: [_Any!]!){_entities(representations: $representations){... on User {reviews {body product {upc __typename}}}}}","variables":{"representations":[{"id":`),
+												Data:        []byte(`{"method":"POST","url":"http://localhost:4002","body":{"query":"query($representations: [_Any!]!){_entities(representations: $representations){... on User {reviews {body product {upc __typename}}}}}","variables":{"representations":[{"id":"`),
 												SegmentType: StaticSegmentType,
 											},
 											{
@@ -2925,7 +2925,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 												Renderer:           NewPlainVariableRendererWithValidation(`{"type":"string"}`),
 											},
 											{
-												Data:        []byte(`,"__typename":"User"}]}}}`),
+												Data:        []byte(`","__typename":"User"}]}}}`),
 												SegmentType: StaticSegmentType,
 											},
 										},
@@ -2977,7 +2977,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 																InputTemplate: InputTemplate{
 																	Segments: []TemplateSegment{
 																		{
-																			Data:        []byte(`{"method":"POST","url":"http://localhost:4003","body":{"query":"query($representations: [_Any!]!){_entities(representations: $representations){... on Product {name}}}","variables":{"representations":[{"upc":`),
+																			Data:        []byte(`{"method":"POST","url":"http://localhost:4003","body":{"query":"query($representations: [_Any!]!){_entities(representations: $representations){... on Product {name}}}","variables":{"representations":[{"upc":"`),
 																			SegmentType: StaticSegmentType,
 																		},
 																		{
@@ -2987,7 +2987,7 @@ func TestResolver_ResolveGraphQLResponse(t *testing.T) {
 																			Renderer:           NewPlainVariableRendererWithValidation(`{"type":"string"}`),
 																		},
 																		{
-																			Data:        []byte(`,"__typename":"Product"}]}}}`),
+																			Data:        []byte(`","__typename":"Product"}]}}}`),
 																			SegmentType: StaticSegmentType,
 																		},
 																	},
@@ -3493,8 +3493,22 @@ func (h hookContextPathMatcher) String() string {
 	return fmt.Sprintf("is equal to %s", h.path)
 }
 
+type initTestVariableRenderer func(jsonSchema string) VariableRenderer
+
+func useTestPlainVariableRenderer() initTestVariableRenderer {
+	return func(jsonSchema string) VariableRenderer {
+		return NewPlainVariableRendererWithValidation(jsonSchema)
+	}
+}
+
+func useTestJSONVariableRenderer() initTestVariableRenderer {
+	return func(jsonSchema string) VariableRenderer {
+		return NewJSONVariableRendererWithValidation(jsonSchema)
+	}
+}
+
 func TestInputTemplate_Render(t *testing.T) {
-	runTest := func(t *testing.T, variables string, sourcePath []string, jsonSchema string, expectErr bool, expected string) {
+	runTest := func(t *testing.T, initRenderer initTestVariableRenderer, variables string, sourcePath []string, jsonSchema string, expectErr bool, expected string) {
 		t.Helper()
 
 		template := InputTemplate{
@@ -3503,7 +3517,7 @@ func TestInputTemplate_Render(t *testing.T) {
 					SegmentType:        VariableSegmentType,
 					VariableKind:       ContextVariableKind,
 					VariableSourcePath: sourcePath,
-					Renderer:           NewPlainVariableRendererWithValidation(jsonSchema),
+					Renderer:           initRenderer(jsonSchema),
 				},
 			},
 		}
@@ -3521,48 +3535,71 @@ func TestInputTemplate_Render(t *testing.T) {
 		assert.Equal(t, expected, out)
 	}
 
-	t.Run("string scalar", func(t *testing.T) {
-		runTest(t, `{"foo":"bar"}`, []string{"foo"}, `{"type":"string"}`, false, `"bar"`)
+	t.Run("plain renderer", func(t *testing.T) {
+		renderer := useTestPlainVariableRenderer()
+		t.Run("string scalar", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":"bar"}`, []string{"foo"}, `{"type":"string"}`, false, `bar`)
+		})
+		t.Run("boolean scalar", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":true}`, []string{"foo"}, `{"type":"boolean"}`, false, "true")
+		})
+		t.Run("nested string", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":"value"}}`, []string{"foo", "bar"}, `{"type":"string"}`, false, `value`)
+		})
+		t.Run("json object pass through", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":"baz"}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"string"}}}`, false, `{"bar":"baz"}`)
+		})
+		t.Run("json object as graphql object", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":"baz"}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"string"}}}`, false, `{"bar":"baz"}`)
+		})
+		t.Run("json object as graphql object with null", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":null}`, []string{"foo"}, `{"type":"string"}`, false, `null`)
+		})
+		t.Run("json object as graphql object with number", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":123}`, []string{"foo"}, `{"type":"integer"}`, false, `123`)
+		})
+		t.Run("json object as graphql object with invalid number", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":123}`, []string{"foo"}, `{"type":"string"}`, true, "")
+		})
+		t.Run("json object as graphql object with boolean", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":true}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"boolean"}}}`, false, `{"bar":true}`)
+		})
+		t.Run("json object as graphql object with number", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":123}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"integer"}}}`, false, `{"bar":123}`)
+		})
+		t.Run("json object as graphql object with float", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":1.23}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"number"}}}`, false, `{"bar":1.23}`)
+		})
+		t.Run("json object as graphql object with nesting", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":{"baz":"bat"}}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"object","properties":{"baz":{"type":"string"}}}}}`, false, `{"bar":{"baz":"bat"}}`)
+		})
+		t.Run("json object as graphql object with single array", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":["bar"]}`, []string{"foo"}, `{"type":"array","item":{"type":"string"}}`, false, `["bar"]`)
+		})
+		t.Run("json object as graphql object with array", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":["bar","baz"]}`, []string{"foo"}, `{"type":"array","item":{"type":"string"}}`, false, `["bar","baz"]`)
+		})
+		t.Run("json object as graphql object with object array", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":[{"bar":"baz"},{"bar":"bat"}]}`, []string{"foo"}, `{"type":"array","item":{"type":"object","properties":{"bar":{"type":"string"}}}}`, false, `[{"bar":"baz"},{"bar":"bat"}]`)
+		})
 	})
-	t.Run("boolean scalar", func(t *testing.T) {
-		runTest(t, `{"foo":true}`, []string{"foo"}, `{"type":"boolean"}`, false, "true")
+
+	t.Run("json renderer", func(t *testing.T) {
+		renderer := useTestJSONVariableRenderer()
+		t.Run("string scalar", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":"bar"}`, []string{"foo"}, `{"type":"string"}`, false, `"bar"`)
+		})
+		t.Run("boolean scalar", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":true}`, []string{"foo"}, `{"type":"boolean"}`, false, "true")
+		})
+		t.Run("number scalar", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":1}`, []string{"foo"}, `{"type":"number"}`, false, "1")
+		})
+		t.Run("nested string", func(t *testing.T) {
+			runTest(t, renderer, `{"foo":{"bar":"value"}}`, []string{"foo", "bar"}, `{"type":"string"}`, false, `"value"`)
+		})
 	})
-	t.Run("json object pass through", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":"baz"}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"string"}}}`, false, `{"bar":"baz"}`)
-	})
-	t.Run("json object as graphql object", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":"baz"}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"string"}}}`, false, `{"bar":"baz"}`)
-	})
-	t.Run("json object as graphql object with null", func(t *testing.T) {
-		runTest(t, `{"foo":null}`, []string{"foo"}, `{"type":"string"}`, false, `null`)
-	})
-	t.Run("json object as graphql object with number", func(t *testing.T) {
-		runTest(t, `{"foo":123}`, []string{"foo"}, `{"type":"integer"}`, false, `123`)
-	})
-	t.Run("json object as graphql object with invalid number", func(t *testing.T) {
-		runTest(t, `{"foo":123}`, []string{"foo"}, `{"type":"string"}`, true, "")
-	})
-	t.Run("json object as graphql object with boolean", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":true}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"boolean"}}}`, false, `{"bar":true}`)
-	})
-	t.Run("json object as graphql object with number", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":123}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"integer"}}}`, false, `{"bar":123}`)
-	})
-	t.Run("json object as graphql object with float", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":1.23}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"number"}}}`, false, `{"bar":1.23}`)
-	})
-	t.Run("json object as graphql object with nesting", func(t *testing.T) {
-		runTest(t, `{"foo":{"bar":{"baz":"bat"}}}`, []string{"foo"}, `{"type":"object","properties":{"bar":{"type":"object","properties":{"baz":{"type":"string"}}}}}`, false, `{"bar":{"baz":"bat"}}`)
-	})
-	t.Run("json object as graphql object with single array", func(t *testing.T) {
-		runTest(t, `{"foo":["bar"]}`, []string{"foo"}, `{"type":"array","item":{"type":"string"}}`, false, `["bar"]`)
-	})
-	t.Run("json object as graphql object with array", func(t *testing.T) {
-		runTest(t, `{"foo":["bar","baz"]}`, []string{"foo"}, `{"type":"array","item":{"type":"string"}}`, false, `["bar","baz"]`)
-	})
-	t.Run("json object as graphql object with object array", func(t *testing.T) {
-		runTest(t, `{"foo":[{"bar":"baz"},{"bar":"bat"}]}`, []string{"foo"}, `{"type":"array","item":{"type":"object","properties":{"bar":{"type":"string"}}}}`, false, `[{"bar":"baz"},{"bar":"bat"}]`)
-	})
+
 	t.Run("array with csv render string", func(t *testing.T) {
 		template := InputTemplate{
 			Segments: []TemplateSegment{

pkg/engine/resolve/variable.go
@@ -117,8 +117,14 @@ func NewPlainVariableRendererWithValidation(jsonSchema string) *PlainVariableRen
 
 // NewPlainVariableRendererWithValidationFromTypeRef creates a new PlainVariableRenderer
 // The argument typeRef must exist on the operation ast.Document, otherwise it will panic!
-func NewPlainVariableRendererWithValidationFromTypeRef(operation, definition *ast.Document, variableTypeRef int) (*PlainVariableRenderer, error) {
-	jsonSchema := graphqljsonschema.FromTypeRef(operation, definition, variableTypeRef)
+func NewPlainVariableRendererWithValidationFromTypeRef(operation, definition *ast.Document, variableTypeRef int, variablePath ...string) (*PlainVariableRenderer, error) {
+	var jsonSchema graphqljsonschema.JsonSchema
+	if len(variablePath) > 1 {
+		jsonSchema = graphqljsonschema.FromTypeRef(operation, definition, variableTypeRef, graphqljsonschema.WithPath(variablePath[1:]))
+	} else {
+		jsonSchema = graphqljsonschema.FromTypeRef(operation, definition, variableTypeRef)
+	}
+
 	validator, err := graphqljsonschema.NewValidatorFromSchema(jsonSchema)
 	if err != nil {
 		return nil, err
@@ -187,7 +193,7 @@ func NewGraphQLVariableRendererFromTypeRef(operation, definition *ast.Document, 
 }
 
 func NewGraphQLVariableRendererFromTypeRefWithOverrides(operation, definition *ast.Document, variableTypeRef int, overrides map[string]graphqljsonschema.JsonSchema) (*GraphQLVariableRenderer, error) {
-	jsonSchema := graphqljsonschema.FromTypeRefWithOverrides(operation, definition, variableTypeRef, overrides)
+	jsonSchema := graphqljsonschema.FromTypeRef(operation, definition, variableTypeRef, graphqljsonschema.WithOverrides(overrides))
 	validator, err := graphqljsonschema.NewValidatorFromSchema(jsonSchema)
 	if err != nil {
 		return nil, err

pkg/graphql/execution_engine_v2_test.go
@@ -418,14 +418,18 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 		},
 	))
 
-	t.Run("execute with .object placeholder", runWithoutError(
+	t.Run("execute with .object and .arguments placeholder", runWithoutError(
 		ExecutionEngineV2TestCase{
 			schema: func(t *testing.T) *Schema {
 				t.Helper()
 				schema := `
 					type Query {
-					  getPet(id: ID): Pet
+					  getPet(id: ID, metadata: APIMetadata): Pet
 					  countries: [Country]
+					}
+
+					input APIMetadata {
+						version: String!
 					}
 					
 					type Country {
@@ -451,8 +455,8 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 				return Request{
 					OperationName: "MyQuery",
 					Query: `
-						query MyQuery {
-						  getPet(id: 1) {
+						query MyQuery($metadata: APIMetadata) {
+						  getPet(id: 1, metadata: $metadata) {
 							id
 							name
 							country {
@@ -461,6 +465,7 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 						  }
 						}
 					`,
+					Variables: []byte(`{"metadata":{"version":"v2"}}`),
 				}
 			},
 			dataSources: []plan.DataSourceConfiguration{
@@ -476,13 +481,13 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 							expectedHost:     "petstore.swagger.io",
 							expectedPath:     "/v2/pet/1",
 							expectedBody:     "",
-							sendResponseBody: `{"id":1,"category":{"id":1,"name":"string"},"name":"doggie"}`,
+							sendResponseBody: `{"id":1,"category":{"id":1,"name":"dog"},"name":"doggie"}`,
 							sendStatusCode:   200,
 						}),
 					},
 					Custom: rest_datasource.ConfigJSON(rest_datasource.Configuration{
 						Fetch: rest_datasource.FetchConfiguration{
-							URL:    "https://petstore.swagger.io/v2/pet/{{.arguments.id}}",
+							URL:    "https://petstore.swagger.io/{{.arguments.metadata.version}}/pet/{{.arguments.id}}",
 							Method: "GET",
 						},
 					}),
@@ -497,7 +502,7 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 					Factory: &rest_datasource.Factory{
 						Client: testNetHttpClient(t, roundTripperTestCase{
 							expectedHost:     "rest-countries.example.com",
-							expectedPath:     "/name/doggie",
+							expectedPath:     "/type/1-dog/name/doggie",
 							expectedBody:     "",
 							sendResponseBody: `{"name":"Germany"}`,
 							sendStatusCode:   200,
@@ -505,7 +510,7 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 					},
 					Custom: rest_datasource.ConfigJSON(rest_datasource.Configuration{
 						Fetch: rest_datasource.FetchConfiguration{
-							URL:    "https://rest-countries.example.com/name/{{.object.name}}",
+							URL:    "https://rest-countries.example.com/type/{{.object.category.id}}-{{.object.category.name}}/name/{{.object.name}}",
 							Method: "POST",
 						},
 					}),
@@ -520,6 +525,10 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 					Arguments: []plan.ArgumentConfiguration{
 						{
 							Name:       "id",
+							SourceType: plan.FieldArgumentSource,
+						},
+						{
+							Name:       "metadata",
 							SourceType: plan.FieldArgumentSource,
 						},
 					},

pkg/graphqljsonschema/jsonschema.go
@@ -12,18 +12,59 @@ import (
 	"github.com/wundergraph/graphql-go-tools/pkg/ast"
 )
 
-func FromTypeRef(operation, definition *ast.Document, typeRef int) JsonSchema {
-	resolver := &fromTypeRefResolver{
-		overrides: map[string]JsonSchema{},
-	}
-	return resolver.fromTypeRef(operation, definition, typeRef)
+type options struct {
+	overrides map[string]JsonSchema
+	path      []string
 }
 
-func FromTypeRefWithOverrides(operation, definition *ast.Document, typeRef int, overrides map[string]JsonSchema) JsonSchema {
-	resolver := &fromTypeRefResolver{
-		overrides: overrides,
+type Option func(opts *options)
+
+func WithOverrides(overrides map[string]JsonSchema) Option {
+	return func(opts *options) {
+		opts.overrides = overrides
 	}
-	return resolver.fromTypeRef(operation, definition, typeRef)
+}
+
+func WithPath(path []string) Option {
+	return func(opts *options) {
+		opts.path = path
+	}
+}
+
+func FromTypeRef(operation, definition *ast.Document, typeRef int, opts ...Option) JsonSchema {
+	appliedOptions := &options{}
+	for _, opt := range opts {
+		opt(appliedOptions)
+	}
+
+	var resolver *fromTypeRefResolver
+	if len(appliedOptions.overrides) > 0 {
+		resolver = &fromTypeRefResolver{
+			overrides: appliedOptions.overrides,
+		}
+	} else {
+		resolver = &fromTypeRefResolver{
+			overrides: map[string]JsonSchema{},
+		}
+	}
+
+	jsonSchema := resolver.fromTypeRef(operation, definition, typeRef)
+	return resolveJsonSchemaPath(jsonSchema, appliedOptions.path)
+}
+
+func resolveJsonSchemaPath(jsonSchema JsonSchema, path []string) JsonSchema {
+	switch typedJsonSchema := jsonSchema.(type) {
+	case Object:
+		for i := 0; i < len(path); i++ {
+			propertyJsonSchema, exists := typedJsonSchema.Properties[path[i]]
+			if !exists {
+				return jsonSchema
+			}
+			jsonSchema = propertyJsonSchema
+		}
+	}
+
+	return jsonSchema
 }
 
 type fromTypeRefResolver struct {

commit 7a80a4d4eaf90d3b7d024a540565836793a1043d

fix planning variable path
regression from https://github.com/wundergraph/graphql-go-tools/pull/373

pkg/engine/plan/plan.go
@@ -1046,13 +1046,14 @@ func (v *Visitor) resolveInputTemplates(config objectFetchConfiguration, input *
 			if !exists {
 				break
 			}
+
 			var variablePath []string
-			switch node.Kind {
-			case ast.NodeKindInputObjectTypeDefinition:
-				variablePath = append(variablePath, path...)
-			default:
-				variablePath = append(variablePath, variableValue)
+			if len(parts) > 2 && node.Kind == ast.NodeKindInputObjectTypeDefinition {
+					variablePath = append(variablePath, path...)
+			} else {
+					variablePath = append(variablePath, variableValue)
 			}
+
 			variable := &resolve.ContextVariable{
 				Path: variablePath,
 			}