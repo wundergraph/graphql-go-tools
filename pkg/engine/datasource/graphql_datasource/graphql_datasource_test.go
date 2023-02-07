7e9dcd80925e793c471df397d8b29cba8dccb245
YuriBuerov <yuribuerov@gmail.com>
2023-02-07 11:53:42 +0100

    chore: fix typo

--- pkg/engine/datasource/graphql_datasource/graphql_datasource_test.go
+++ pkg/engine/datasource/graphql_datasource/graphql_datasource_test.go
@@ -8150,20 +8150,19 @@ func TestSource_Load(t *testing.T) {
 		var (
 			src       = &Source{httpClient: &http.Client{}}
 			serverUrl = ts.URL
-			variables = []byte(`{"a":null,"b":"null"}`)
+			variables = []byte(`{"a":null,"b":null}`)
 		)
 		t.Run("should remove undefined variables and leave null variables", func(t *testing.T) {
 			var input []byte
 			input = httpclient.SetInputBodyWithPath(input, variables, "variables")
 			input = httpclient.SetInputURL(input, []byte(serverUrl))
-			input = httpclient.SetInputFlag(input, httpclient.UNNULLVARIABLES)
 			buf := bytes.NewBuffer(nil)
 
 			// consider variable a undefined
 			ctx := httpclient.CtxSetUndefinedVariables(context.Background(), []string{"a"})
 
 			require.NoError(t, src.Load(ctx, input, buf))
-			assert.Equal(t, `{"variables":{"b":"null"}}`, buf.String())
+			assert.Equal(t, `{"variables":{"b":null}}`, buf.String())
 		})
 	})
 }
