9d83dacb510065313530b23df471ae45831180d6
spetrunin <neyasut@gmail.com>
2023-08-24 16:59:44 +0300

    new datasource filter

--- v2/pkg/engine/plan/datasource_filter_visitor.go
+++ v2/pkg/engine/plan/datasource_filter_visitor.go
@@ -1,58 +1,149 @@
 package plan
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/ast"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/astvisitor"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
 )
 
 func FilterDataSources(operation, definition *ast.Document, report *operationreport.Report, dataSources []DataSourceConfiguration) []DataSourceConfiguration {
-	walker := astvisitor.NewWalker(48)
+	return nil
+}
 
-	ds := make([]*DataSourceConfiguration, 0, len(dataSources))
-	for _, source := range dataSources {
-		s := source
-		ds = append(ds, &s)
+type UsedNode struct {
+	TypeName  string
+	FieldName string
+}
+
+type UsedDataSourceConfiguration struct {
+	DataSource DataSourceConfiguration
+	UsedNodes  []*UsedNode
+}
+
+type dataSourceVisitor struct {
+	operation   *ast.Document
+	definition  *ast.Document
+	walker      *astvisitor.Walker
+	dataSources []*UsedDataSourceConfiguration
+	err         error
+}
+
+func hasNode(f []TypeField, typeName, fieldName string) bool {
+	for i := range f {
+		if typeName != f[i].TypeName {
+			continue
+		}
+		for j := range f[i].FieldNames {
+			if fieldName == f[i].FieldNames[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (v *dataSourceVisitor) EnterField(ref int) {
+	if v.err != nil {
+		return
+	}
+	typeName := v.walker.EnclosingTypeDefinition.NameString(v.definition)
+	fieldName := v.operation.FieldNameUnsafeString(ref)
+	found := false
+	for _, v := range v.dataSources {
+		ds := v.DataSource
+		if ds.HasRootNode(typeName, fieldName) || hasNode(ds.ChildNodes, typeName, fieldName) {
+			v.UsedNodes = append(v.UsedNodes, &UsedNode{
+				TypeName:  typeName,
+				FieldName: fieldName,
+			})
+			found = true
+			break
+		}
 	}
 
-	visitor := &UsedDatasourcesVisitor{
-		walker:               &walker,
-		dataSources:          ds,
-		operation:            operation,
-		definition:           definition,
-		filteredDataSourcess: make(map[*DataSourceConfiguration]struct{}),
+	if !found {
+		v.err = &errOperationFieldNotResolved{TypeName: typeName, FieldName: fieldName}
+	}
+}
+
+type errOperationFieldNotResolved struct {
+	TypeName  string
+	FieldName string
+}
+
+func (e *errOperationFieldNotResolved) Error() string {
+	return fmt.Sprintf("could not resolve %s.%s", e.TypeName, e.FieldName)
+}
+
+func planDataSources(operation *ast.Document, definition *ast.Document, report *operationreport.Report, dataSources []DataSourceConfiguration) ([]*UsedDataSourceConfiguration, error) {
+	if report == nil {
+		panic("report can't be nil")
+	}
+	walker := astvisitor.NewWalker(32)
+	dataSourcesToVisit := make([]*UsedDataSourceConfiguration, len(dataSources))
+	for ii, v := range dataSources {
+		v := v
+		dataSourcesToVisit[ii] = &UsedDataSourceConfiguration{
+			DataSource: v,
+		}
+	}
+	visitor := &dataSourceVisitor{
+		operation:   operation,
+		definition:  definition,
+		walker:      &walker,
+		dataSources: dataSourcesToVisit,
 	}
 	walker.RegisterEnterFieldVisitor(visitor)
 	walker.Walk(operation, definition, report)
 	if report.HasErrors() {
-		return nil
+		return nil, errors.New(report.Error())
 	}
-
-	return visitor.Results()
-}
-
-type UsedDatasourcesVisitor struct {
-	walker                *astvisitor.Walker
-	operation, definition *ast.Document
-	dataSources           []*DataSourceConfiguration
-	filteredDataSourcess  map[*DataSourceConfiguration]struct{}
-}
-
-func (u *UsedDatasourcesVisitor) Results() []DataSourceConfiguration {
-	out := make([]DataSourceConfiguration, 0, len(u.filteredDataSourcess))
-	for ds := range u.filteredDataSourcess {
-		out = append(out, *ds)
+	if visitor.err != nil {
+		return nil, visitor.err
 	}
-	return out
-}
-
-func (u *UsedDatasourcesVisitor) EnterField(ref int) {
-	fieldName := u.operation.FieldNameUnsafeString(ref)
-	typeName := u.walker.EnclosingTypeDefinition.NameString(u.definition)
-
-	for _, config := range u.dataSources {
-		if config.HasRootNode(typeName, fieldName) || config.HasChildNode(typeName, fieldName) {
-			u.filteredDataSourcess[config] = struct{}{}
+	var usedDataSources []*UsedDataSourceConfiguration
+	for _, v := range dataSourcesToVisit {
+		if len(v.UsedNodes) > 0 {
+			usedDataSources = append(usedDataSources, v)
 		}
 	}
+	return usedDataSources, nil
+}
+
+func PlanDataSources(operation *ast.Document, definition *ast.Document, report *operationreport.Report, dataSources []DataSourceConfiguration) ([]*UsedDataSourceConfiguration, error) {
+	if report == nil {
+		report = &operationreport.Report{}
+	}
+	planned, err := planDataSources(operation, definition, report, dataSources)
+	if err != nil {
+		return nil, err
+	}
+	if len(planned) == 1 {
+		return planned, nil
+	}
+	best := planned
+	for excluded := range dataSources {
+		subset := make([]DataSourceConfiguration, 0, len(dataSources)-1)
+		for ii, ds := range dataSources {
+			if ii != excluded {
+				subset = append(subset, ds)
+			}
+		}
+		result, err := PlanDataSources(operation, definition, report, subset)
+		if err != nil {
+			var rerr *errOperationFieldNotResolved
+			if errors.As(err, &rerr) {
+				// We removed a data source that causes the resolution to fail
+				continue
+			}
+			return nil, err
+		}
+		if result != nil && len(result) < len(best) {
+			best = result
+		}
+	}
+	return best, nil
 }
--- v2/pkg/engine/plan/datasource_filter_visitor_test.go
+++ v2/pkg/engine/plan/datasource_filter_visitor_test.go
@@ -1,66 +1,252 @@
-// BEGIN: 8f7e6d5a7b3c
-package plan
+package main
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/wundergraph/graphql-go-tools/v2/internal/pkg/unsafeparser"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/astparser"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/asttransform"
-	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
 )
 
-func TestFilterDataSources(t *testing.T) {
-	definition := unsafeparser.ParseGraphqlDocumentString("type Query {user: User} type User {age: Int  name: String}")
-	_ = asttransform.MergeDefinitionWithBaseSchema(&definition)
+type dsBuilder struct {
+	ds DataSourceConfiguration
+}
 
-	operation := unsafeparser.ParseGraphqlDocumentString("{ user {age} }")
+func dsb() *dsBuilder { return &dsBuilder{} }
 
-	dataSources := []DataSourceConfiguration{
+func (b *dsBuilder) RootNodeFields(typeName string, fieldNames []string) *dsBuilder {
+	b.ds.RootNodes = append(b.ds.RootNodes, TypeField{TypeName: typeName, FieldNames: fieldNames})
+	return b
+}
+
+func (b *dsBuilder) RootNode(typeName string, fieldName string) *dsBuilder {
+	return b.RootNodeFields(typeName, []string{fieldName})
+}
+
+func (b *dsBuilder) ChildNodeFields(typeName string, fieldNames []string) *dsBuilder {
+	b.ds.ChildNodes = append(b.ds.ChildNodes, TypeField{TypeName: typeName, FieldNames: fieldNames})
+	return b
+}
+
+func (b *dsBuilder) ChildNode(typeName string, fieldName string) *dsBuilder {
+	return b.ChildNodeFields(typeName, []string{fieldName})
+}
+
+func (b *dsBuilder) DS() DataSourceConfiguration {
+	return b.ds
+}
+
+type expectedDataSource struct {
+	Index     int
+	UsedNodes []*UsedNode
+}
+
+func TestVisitDataSource(t *testing.T) {
+	testCases := []struct {
+		Definition  string
+		Query       string
+		DataSources []DataSourceConfiguration
+		Expected    []expectedDataSource
+	}{
+		// Remove the 3rd data source, we don't use it
 		{
-			RootNodes: []TypeField{
-				{
-					TypeName:   "Query",
-					FieldNames: []string{"user"},
-				},
+			Definition: `
+				type Query {
+					user: User
+				}
+				type User {
+					id: Int
+					name: String
+					surname: String
+				}	
+			`,
+			Query: `
+				query {
+					user {
+						id
+						name
+						surname
+					}
+				}
+			`,
+			DataSources: []DataSourceConfiguration{
+				dsb().RootNode("Query", "user").ChildNode("User", "id").DS(),
+				dsb().RootNode("User", "name").RootNode("User", "surname").DS(),
+				dsb().RootNode("User", "age").DS(),
 			},
-			ChildNodes: []TypeField{
+			Expected: []expectedDataSource{
 				{
-					TypeName:   "User",
-					FieldNames: []string{"age"},
+					Index:     0,
+					UsedNodes: []*UsedNode{{"Query", "user"}, {"User", "id"}},
+				},
+				{
+					Index:     1,
+					UsedNodes: []*UsedNode{{"User", "name"}, {"User", "surname"}},
 				},
 			},
 		},
+		// Pick the first and the third data sources, ignore the ones that result in more queries
 		{
-			RootNodes: []TypeField{
+			Definition: `
+				type Query {
+					user: User
+				}
+				type User {
+					age: Int
+					name: String
+					surname: String
+				}	
+			`,
+			Query: `
+				query {
+					user {
+						age
+						name
+						surname
+					}
+				}
+			`,
+			DataSources: []DataSourceConfiguration{
+				dsb().RootNode("Query", "user").ChildNode("User", "age").DS(),
+				dsb().RootNode("User", "age").RootNode("User", "name").DS(),
+				dsb().RootNode("User", "name").RootNode("User", "surname").DS(),
+				dsb().RootNode("User", "age").DS(),
+				dsb().RootNode("User", "name").DS(),
+			},
+			Expected: []expectedDataSource{
 				{
-					TypeName:   "User",
-					FieldNames: []string{"name"},
+					Index:     0,
+					UsedNodes: []*UsedNode{{"Query", "user"}, {"User", "age"}},
+				},
+				{
+					Index:     2,
+					UsedNodes: []*UsedNode{{"User", "name"}, {"User", "surname"}},
+				},
+			},
+		},
+		// Initial example from SVG
+		{
+			Definition: `
+						type Query {
+							user: User
+						}
+						type User {
+							name: String
+							details: Details
+						}
+						type Details {
+							age: Int
+							address: Address
+						}
+						type Address {
+							name: String
+							lines: Lines
+						}
+						type Lines {
+							line1: String
+							line2: String
+						}
+					`,
+			Query: `
+						query {
+							user {
+								name
+								details {
+									age
+									address {
+										name
+										lines {
+											line1
+											line2
+										}
+									}
+								}
+							}
+						}
+					`,
+			DataSources: []DataSourceConfiguration{
+				// sub1
+				dsb().
+					RootNode("Query", "user").
+					ChildNode("User", "name").DS(),
+				// sub2
+				dsb().
+					RootNode("Query", "user").
+					ChildNode("Details", "address").
+					ChildNode("User", "details").
+					ChildNode("User", "name").
+					ChildNode("Details", "age").
+					ChildNode("Details", "address").
+					ChildNode("Address", "name").
+					ChildNode("Address", "lines").
+					DS(),
+				// sub3
+				dsb().
+					ChildNode("Details", "address").
+					ChildNode("Address", "lines").
+					ChildNode("Lines", "line1").
+					DS(),
+				// sub4
+				dsb().
+					RootNode("Lines", "line1").
+					RootNode("Lines", "line2").DS(),
+			},
+			Expected: []expectedDataSource{
+				{
+					Index: 1,
+					UsedNodes: []*UsedNode{
+						{"Query", "user"},
+						{"User", "name"},
+						{"User", "details"},
+						{"Details", "age"},
+						{"Details", "address"},
+						{"Address", "name"},
+						{"Address", "lines"},
+					},
+				},
+				{
+					Index: 3,
+					UsedNodes: []*UsedNode{
+						{"Lines", "line1"},
+						{"Lines", "line2"},
+					},
 				},
 			},
 		},
 	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Query, func(t *testing.T) {
+			// t.Parallel()
 
-	expectedDataSources := []DataSourceConfiguration{
-		{
-			RootNodes: []TypeField{
-				{
-					TypeName:   "Query",
-					FieldNames: []string{"user"},
-				},
-			},
-			ChildNodes: []TypeField{
-				{
-					TypeName:   "User",
-					FieldNames: []string{"age"},
-				},
-			},
-		},
+			definition, report := astparser.ParseGraphqlDocumentString(tc.Definition)
+			if report.HasErrors() {
+				t.Fatal(report.Error())
+			}
+			if err := asttransform.MergeDefinitionWithBaseSchema(&definition); err != nil {
+				t.Fatal(err)
+			}
+			operation, report := astparser.ParseGraphqlDocumentString(tc.Query)
+			if report.HasErrors() {
+				t.Fatal(report.Error())
+			}
+			planned, err := PlanDataSources(&operation, &definition, &report, tc.DataSources)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if report.HasErrors() {
+				t.Fatal(report.Error())
+			}
+			var expected []*UsedDataSourceConfiguration
+			for _, exp := range tc.Expected {
+				expected = append(expected, &UsedDataSourceConfiguration{
+					DataSource: tc.DataSources[exp.Index],
+					UsedNodes:  exp.UsedNodes,
+				})
+			}
+			assert.Equal(t, expected, planned)
+		})
 	}
 
-	report := operationreport.Report{}
-	result := FilterDataSources(&operation, &definition, &report, dataSources)
-	assert.False(t, report.HasErrors())
-	assert.Equal(t, expectedDataSources, result)
 }
