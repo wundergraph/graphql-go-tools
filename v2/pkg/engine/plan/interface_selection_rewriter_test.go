e35003bfc73f106347195a848741ce99b79176a1
spetrunin <neyasut@gmail.com>
2023-10-15 22:39:25 +0300

    add more test cases

--- v2/pkg/engine/plan/interface_selection_rewriter_test.go
+++ v2/pkg/engine/plan/interface_selection_rewriter_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestInterfaceSelectionRewriter_RewriteOperation(t *testing.T) {
-	run := func(t *testing.T, definition string, dsConfiguration *DataSourceConfiguration, operation string, expectedOperation string) {
+	run := func(t *testing.T, definition string, dsConfiguration *DataSourceConfiguration, operation string, expectedOperation string, expectedRewritten bool) {
 		t.Helper()
 
 		op := unsafeparser.ParseGraphqlDocumentString(operation)
@@ -28,7 +28,9 @@ func TestInterfaceSelectionRewriter_RewriteOperation(t *testing.T) {
 		node, _ := def.Index.FirstNodeByNameStr("Query")
 
 		rewriter := newInterfaceSelectionRewriter(&op, &def)
-		rewriter.RewriteOperation(fieldRef, node, dsConfiguration)
+		rewrittern, err := rewriter.RewriteOperation(fieldRef, node, dsConfiguration)
+		require.NoError(t, err)
+		require.Equal(t, expectedRewritten, rewrittern)
 
 		printedOp := unsafeprinter.PrettyPrint(&op, &def)
 		expectedPretty := unsafeprinter.Prettify(expectedOperation)
@@ -36,64 +38,274 @@ func TestInterfaceSelectionRewriter_RewriteOperation(t *testing.T) {
 		require.Equal(t, expectedPretty, printedOp)
 	}
 
-	t.Run("simple", func(t *testing.T) {
-		definition := `
-			interface Node {
-				id: ID!
-				name: String!
-			}
-			
-			type User implements Node {
-				id: ID!
-				name: String!
-			}
+	type testCase struct {
+		name              string
+		definition        string
+		dsConfiguration   *DataSourceConfiguration
+		operation         string
+		expectedOperation string
+		expectedRewritten bool
+	}
 
-			type Admin implements Node {
-				id: ID!
-				name: String! @external
-			}
-			
-			type Query {
-				iface: Node!
-			}
-		`
-
-		operation := `
-			query {
-				iface {
-					name
-				}
-			}`
-
-		expectedOperation := `
-			query {
-				iface {
-					... on User {
-						name
-					}
-					... on Admin {
-						name
-					}
-				}
-			}
-		`
-
-		dsConfiguration := dsb().
-			RootNode("Query", "iface").
-			RootNode("User", "id", "name").
-			RootNode("Admin", "id").ds
-
-		dsConfiguration.FederationMetaData.Keys = FederationFieldConfigurations{
-			{
-				TypeName:     "User",
-				SelectionSet: "id",
-			},
-			{
-				TypeName:     "Admin",
-				SelectionSet: "id",
-			},
+	definition := `
+		interface Node {
+			id: ID!
+			name: String!
+		}
+		
+		type User implements Node {
+			id: ID!
+			name: String!
+			isUser: Boolean!
 		}
 
-		run(t, definition, dsConfiguration, operation, expectedOperation)
-	})
+		type Admin implements Node {
+			id: ID!
+			name: String!
+		}
+		
+		type Query {
+			iface: Node!
+		}
+	`
+
+	testCases := []testCase{
+		{
+			name:       "one field is external. query without fragments",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				RootNode("User", "id", "name", "isUser").
+				RootNode("Admin", "id").
+				KeysMetadata(FederationFieldConfigurations{
+					{
+						TypeName:     "User",
+						SelectionSet: "id",
+					},
+					{
+						TypeName:     "Admin",
+						SelectionSet: "id",
+					},
+				}).
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						... on User {
+							name
+						}
+						... on Admin {
+							name
+						}
+					}
+				}`,
+			expectedRewritten: true,
+		},
+		{
+			name:       "one field is external. query has user fragment",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				RootNode("User", "id", "name", "isUser").
+				RootNode("Admin", "id").
+				KeysMetadata(FederationFieldConfigurations{
+					{
+						TypeName:     "User",
+						SelectionSet: "id",
+					},
+					{
+						TypeName:     "Admin",
+						SelectionSet: "id",
+					},
+				}).
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						... on Admin {
+							name
+						}
+						... on User {
+							isUser
+							name
+						}
+					}
+				}`,
+			expectedRewritten: true,
+		},
+		{
+			name:       "one field is external. query has admin and user fragment",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				RootNode("User", "id", "isUser").
+				RootNode("Admin", "id").
+				KeysMetadata(FederationFieldConfigurations{
+					{
+						TypeName:     "User",
+						SelectionSet: "id",
+					},
+					{
+						TypeName:     "Admin",
+						SelectionSet: "id",
+					},
+				}).
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+						... on Admin {
+							id
+						}
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						... on User {
+							isUser
+							name
+						}
+						... on Admin {
+							id
+							name
+						}
+					}
+				}`,
+			expectedRewritten: true,
+		},
+		{
+			name:       "all fields local. query without fragments",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				RootNode("User", "id", "name").
+				RootNode("Admin", "id", "name").
+				KeysMetadata(FederationFieldConfigurations{
+					{
+						TypeName:     "User",
+						SelectionSet: "id",
+					},
+					{
+						TypeName:     "Admin",
+						SelectionSet: "id",
+					},
+				}).
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						name
+					}
+				}`,
+			expectedRewritten: false,
+		},
+		{
+			name:       "all fields local. query has user fragment",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				RootNode("User", "id", "name", "isUser").
+				RootNode("Admin", "id", "name").
+				KeysMetadata(FederationFieldConfigurations{
+					{
+						TypeName:     "User",
+						SelectionSet: "id",
+					},
+					{
+						TypeName:     "Admin",
+						SelectionSet: "id",
+					},
+				}).
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+					}
+				}`,
+			expectedRewritten: false,
+		},
+		{
+			name:       "all fields local. query without fragment. types are not entities",
+			definition: definition,
+			dsConfiguration: dsb().
+				RootNode("Query", "iface").
+				ChildNode("User", "id", "name", "isUser").
+				ChildNode("Admin", "id", "name").
+				DSPtr(),
+			operation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+					}
+				}`,
+
+			expectedOperation: `
+				query {
+					iface {
+						name
+						... on User {
+							isUser
+						}
+					}
+				}`,
+			expectedRewritten: false,
+		},
+
+		// field is not an interface
+		// fragment already had a shared field
+		// all fragments already had shared fields
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			run(t, testCase.definition, testCase.dsConfiguration, testCase.operation, testCase.expectedOperation, testCase.expectedRewritten)
+		})
+	}
 }
