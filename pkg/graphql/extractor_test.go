19d5397cf8c52b109d42fd2ac038c19d62083348
Kofo Okesola <okesolakofo@gmail.com>
2023-11-03 17:29:05 +0100

    added limit for operation

--- pkg/graphql/extractor_test.go
+++ pkg/graphql/extractor_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestExtractor_ExtractFieldsFromRequest(t *testing.T) {
+
 	schema, err := NewSchemaFromString(testDefinition)
 	require.NoError(t, err)
 
@@ -19,49 +20,79 @@ func TestExtractor_ExtractFieldsFromRequest(t *testing.T) {
 		Query:         testOperation,
 	}
 
-	fields := make(RequestTypes)
-	report := operationreport.Report{}
-	NewExtractor().ExtractFieldsFromRequest(&request, schema, &report, fields)
+	t.Run("no operation limit", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+		NewExtractor().ExtractFieldsFromRequest(&request, schema, &report, fields)
 
-	expectedFields := RequestTypes{
-		"Foo":   {"fooField": {}},
-		"Post":  {"description": {}, "id": {}, "user": {}},
-		"Query": {"foo": {}, "posts": {}},
-		"User":  {"id": {}, "name": {}},
-	}
+		expectedFields := RequestTypes{
+			"Foo":   {"fooField": {}},
+			"Post":  {"description": {}, "id": {}, "user": {}},
+			"Query": {"foo": {}, "posts": {}},
+			"User":  {"id": {}, "name": {}},
+		}
 
-	assert.False(t, report.HasErrors())
-	assert.Equal(t, expectedFields, fields)
+		assert.False(t, report.HasErrors())
+		assert.Equal(t, expectedFields, fields)
+	})
+
+	t.Run("with operation limit", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+
+		NewExtractor().ExtractFieldsFromRequestSingleOperation(&request, request.OperationName, schema, &report, fields)
+		expectedFields := RequestTypes{
+			"Post":  {"description": {}, "id": {}, "user": {}},
+			"Query": {"posts": {}},
+			"User":  {"id": {}, "name": {}},
+		}
+		assert.Equal(t, expectedFields, fields)
+	})
+
+	t.Run("with operation limit no operation", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+		request.OperationName = ""
+
+		NewExtractor().ExtractFieldsFromRequestSingleOperation(&request, request.OperationName, schema, &report, fields)
+		expectedFields := RequestTypes{
+			"Foo":   {"fooField": {}},
+			"Query": {"foo": {}},
+		}
+		assert.Equal(t, expectedFields, fields)
+	})
 }
 
-const testOperation = `query PostsUserQuery {
-	posts {
-		id
-		description
-		user {
-			id
-			name
-		}
-	}
+const testOperation = `query ArgsQuery {
+  foo(bar: "barValue", baz: true) {
+    fooField
+  }
+}
+query PostsUserQuery {
+  posts {
+    id
+    description
+    user {
+      id
+      name
+    }
+  }
 }
 fragment FirstFragment on Post {
-	id
+  id
 }
-query ArgsQuery {
-	foo(bar: "barValue", baz: true){
-		fooField
-	}
-}
+
 query VariableQuery($bar: String, $baz: Boolean) {
-	foo(bar: $bar, baz: $baz){
-		fooField
-	}
+  foo(bar: $bar, baz: $baz) {
+    fooField
+  }
 }
 query VariableQuery {
-	posts {
-		id @include(if: true)
-	}
+  posts {
+    id @include(if: true)
+  }
 }
+
 `
 
 const testDefinition = `
