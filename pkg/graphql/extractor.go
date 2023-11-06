27618ae672d3a5604b137802f42be4a08eb17eac
Kofo Okesola <okesolakofo@gmail.com>
2023-11-06 11:07:46 +0100

    [TT-9467]: added limit for operation in graphql extractor (#396)
    [TT-9467](https://tyktech.atlassian.net/browse/TT-9476)
    
    [TT-9467]:
    https://tyktech.atlassian.net/browse/TT-9467?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- pkg/graphql/extractor.go
+++ pkg/graphql/extractor.go
@@ -1,6 +1,8 @@
 package graphql
 
 import (
+	"bytes"
+
 	"github.com/TykTechnologies/graphql-go-tools/pkg/ast"
 	"github.com/TykTechnologies/graphql-go-tools/pkg/astvisitor"
 	"github.com/TykTechnologies/graphql-go-tools/pkg/operationreport"
@@ -18,6 +20,7 @@ func NewExtractor() *Extractor {
 	}
 
 	walker.RegisterEnterFieldVisitor(&visitor)
+	walker.RegisterEnterOperationVisitor(&visitor)
 
 	return &Extractor{
 		walker:  &walker,
@@ -43,13 +46,45 @@ func (e *Extractor) ExtractFieldsFromRequest(request *Request, schema *Schema, r
 	e.walker.Walk(&request.document, &schema.document, report)
 }
 
+func (e *Extractor) ExtractFieldsFromRequestSingleOperation(request *Request, schema *Schema, report *operationreport.Report, data RequestTypes) {
+	e.visitor.singleOperation = true
+	e.visitor.operationName = request.OperationName
+
+	e.ExtractFieldsFromRequest(request, schema, report, data)
+}
+
 type requestVisitor struct {
+	singleOperation, shouldSkip bool
+	operationName               string
 	*astvisitor.Walker
 	operation, definition *ast.Document
 	data                  RequestTypes
 }
 
+func (p *requestVisitor) EnterOperationDefinition(ref int) {
+	if !p.singleOperation {
+		return
+	}
+	if p.operationName == "" && ref == 0 {
+		p.shouldSkip = false
+		return
+	} else if p.operationName == "" && ref != 0 {
+		p.shouldSkip = true
+		return
+	}
+
+	opName := p.operation.OperationDefinitionNameBytes(ref)
+	if bytes.Equal(opName, []byte(p.operationName)) {
+		p.shouldSkip = false
+	} else {
+		p.shouldSkip = true
+	}
+}
+
 func (p *requestVisitor) EnterField(ref int) {
+	if p.shouldSkip {
+		return
+	}
 	fieldName := p.operation.FieldNameString(ref)
 	parentTypeName := p.definition.NodeNameString(p.EnclosingTypeDefinition)
 
--- pkg/graphql/extractor_test.go
+++ pkg/graphql/extractor_test.go
@@ -19,49 +19,94 @@ func TestExtractor_ExtractFieldsFromRequest(t *testing.T) {
 		Query:         testOperation,
 	}
 
-	fields := make(RequestTypes)
-	report := operationreport.Report{}
-	NewExtractor().ExtractFieldsFromRequest(&request, schema, &report, fields)
+	t.Run("no operation limit", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+		NewExtractor().ExtractFieldsFromRequest(&request, schema, &report, fields)
 
-	expectedFields := RequestTypes{
-		"Foo":   {"fooField": {}},
-		"Post":  {"description": {}, "id": {}, "user": {}},
-		"Query": {"foo": {}, "posts": {}},
-		"User":  {"id": {}, "name": {}},
-	}
+		expectedFields := RequestTypes{
+			"Foo":   {"fooField": {}},
+			"Post":  {"description": {}, "id": {}, "user": {}},
+			"Query": {"foo": {}, "posts": {}},
+			"User":  {"id": {}, "name": {}},
+		}
 
-	assert.False(t, report.HasErrors())
-	assert.Equal(t, expectedFields, fields)
+		assert.False(t, report.HasErrors())
+		assert.Equal(t, expectedFields, fields)
+	})
+
+	t.Run("with operation limit", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+
+		NewExtractor().ExtractFieldsFromRequestSingleOperation(&request, schema, &report, fields)
+		expectedFields := RequestTypes{
+			"Post":  {"description": {}, "id": {}, "user": {}},
+			"Query": {"posts": {}},
+			"User":  {"id": {}, "name": {}},
+		}
+		assert.Equal(t, expectedFields, fields)
+	})
+
+	t.Run("with operation limit no operation", func(t *testing.T) {
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+		request.OperationName = ""
+
+		NewExtractor().ExtractFieldsFromRequestSingleOperation(&request, schema, &report, fields)
+		expectedFields := RequestTypes{
+			"Foo":   {"fooField": {}},
+			"Query": {"foo": {}},
+		}
+		assert.Equal(t, expectedFields, fields)
+	})
+
+	t.Run("with operation limit no operation custom query", func(t *testing.T) {
+		schema, err := NewSchemaFromString(testDefinitionCustomQuery)
+		require.NoError(t, err)
+		fields := make(RequestTypes)
+		report := operationreport.Report{}
+		request.OperationName = ""
+
+		NewExtractor().ExtractFieldsFromRequestSingleOperation(&request, schema, &report, fields)
+		expectedFields := RequestTypes{
+			"Foo":         {"fooField": {}},
+			"CustomQuery": {"foo": {}},
+		}
+		assert.Equal(t, expectedFields, fields)
+	})
 }
 
-const testOperation = `query PostsUserQuery {
-	posts {
-		id
-		description
-		user {
-			id
-			name
-		}
-	}
+const testOperation = `query ArgsQuery {
+  foo(bar: "barValue", baz: true) {
+    fooField
+  }
+}
+query PostsUserQuery {
+  posts {
+    id
+    description
+    user {
+      id
+      name
+    }
+  }
 }
 fragment FirstFragment on Post {
-	id
+  id
 }
-query ArgsQuery {
-	foo(bar: "barValue", baz: true){
-		fooField
-	}
-}
+
 query VariableQuery($bar: String, $baz: Boolean) {
-	foo(bar: $bar, baz: $baz){
-		fooField
-	}
+  foo(bar: $bar, baz: $baz) {
+    fooField
+  }
 }
 query VariableQuery {
-	posts {
-		id @include(if: true)
-	}
+  posts {
+    id @include(if: true)
+  }
 }
+
 `
 
 const testDefinition = `
@@ -89,3 +134,28 @@ type Foo {
 scalar ID
 scalar String
 `
+
+const testDefinitionCustomQuery = `
+directive @include(if: Boolean!) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+schema {
+	query: CustomQuery
+}
+type CustomQuery {
+	posts: [Post]
+	foo(bar: String!, baz: Boolean!): Foo
+}
+type User {
+	id: ID
+	name: String
+	posts: [Post]
+}
+type Post {
+	id: ID
+	description: String
+	user: User
+}
+type Foo {
+	fooField: String
+}
+scalar ID
+scalar String`
