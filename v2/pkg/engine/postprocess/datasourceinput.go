3368387ffeeeec2b5e028ef264411ee463269e61
jackchuka <jack16.m.u@gmail.com>
2023-11-06 21:24:48 +0900

    fix: SortFunc

--- v2/pkg/engine/postprocess/datasourceinput.go
+++ v2/pkg/engine/postprocess/datasourceinput.go
@@ -51,7 +51,7 @@ func (d *ProcessDataSource) traverseFetch(fetch resolve.Fetch) {
 		slices.SortFunc(f.Fetches, func(a, b resolve.Fetch) int {
 			// serial fetch always has a single fetch as child
 			// sort descending by serial id
-			return a.(*resolve.SingleFetch).SerialID - b.(*resolve.SingleFetch).SerialID
+			return b.(*resolve.SingleFetch).SerialID - a.(*resolve.SingleFetch).SerialID
 		})
 		for i := range f.Fetches {
 			d.traverseFetch(f.Fetches[i])
