624782c2949697e918aa6e44758d61e6c819755a
fiam <alberto@garciahierro.com>
2023-11-15 21:27:29 +0000

    fix: race condition in resolver when the data source is cancelled
    If the source get cancelled while resolver had just checked the Context
    and started to receive the next payload from the data source we would
    get stuck. Instead of using a default in the select, select between the
    Done() channel and the channel receiving payload. This way any cancellation
    will immediately stop blocking trying to receive the next payload.

--- v2/pkg/engine/resolve/resolve.go
+++ v2/pkg/engine/resolve/resolve.go
@@ -120,8 +120,7 @@ func (r *Resolver) ResolveGraphQLSubscription(ctx *Context, subscription *GraphQ
 		select {
 		case <-resolverDone:
 			return nil
-		default:
-			data, ok := <-next
+		case data, ok := <-next:
 			if !ok {
 				return nil
 			}
