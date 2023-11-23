1e4940bf0dbf9e4c577bec6d950fbc69b91db3e5
spetrunin <neyasut@gmail.com>
2023-11-23 09:55:12 +0200

    fix tests

--- v2/pkg/graphql/execution_engine_v2_test.go
+++ v2/pkg/graphql/execution_engine_v2_test.go
@@ -668,7 +668,7 @@ func TestExecutionEngineV2_Execute(t *testing.T) {
 				},
 			},
 		},
-		expectedResponse: `{"errors":[{"message":"Cannot return null for non-nullable field Query.hero.","path":["hero"]}],"data":null}`,
+		expectedResponse: `{"errors":[{"message":"Cannot return null for non-nullable field 'Query.hero'","path":["hero"]}],"data":null}`,
 	}))
 
 	t.Run("execute operation and apply input coercion for lists without variables", runWithoutError(ExecutionEngineV2TestCase{
