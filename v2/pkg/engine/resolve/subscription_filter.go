62efcf44f1c5cd436cab7f71b763a9fb3ef1b9cb
Jens Neuse <jens.neuse@gmx.de>
2024-05-08 13:50:38 +0200

    feat: implement subscriptions filter in resolver

---
+++ v2/pkg/engine/resolve/subscription_filter.go
@@ -0,0 +1,133 @@
+package resolve
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+
+	"github.com/buger/jsonparser"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/lexer/literal"
+)
+
+type SubscriptionFilter struct {
+	And []*SubscriptionFilter
+	Or  []*SubscriptionFilter
+	Not *SubscriptionFilter
+	In  *SubscriptionFieldFilter
+}
+
+type SubscriptionFieldFilter struct {
+	FieldPath []string
+	Values    []InputTemplate
+}
+
+func (f *SubscriptionFilter) SkipEvent(ctx *Context, data []byte, buf *bytes.Buffer) (bool, error) {
+	if f == nil {
+		return false, nil
+	}
+
+	if f.And != nil {
+		for _, filter := range f.And {
+			skip, err := filter.SkipEvent(ctx, data, buf)
+			if err != nil {
+				return false, err
+			}
+			if skip {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+
+	if f.Or != nil {
+		for _, filter := range f.Or {
+			skip, err := filter.SkipEvent(ctx, data, buf)
+			if err != nil {
+				return false, err
+			}
+			if !skip {
+				return false, nil
+			}
+		}
+		return true, nil
+	}
+
+	if f.Not != nil {
+		skip, err := f.Not.SkipEvent(ctx, data, buf)
+		if err != nil {
+			return false, err
+		}
+		return !skip, nil
+	}
+
+	if f.In != nil {
+		return f.In.SkipEvent(ctx, data, buf)
+	}
+
+	return false, nil
+}
+
+var (
+	findArray                         = regexp.MustCompile(`\[(.*?)\]`)
+	InvalidSubscriptionFilterTemplate = fmt.Errorf("invalid subscription filter template")
+)
+
+func (f *SubscriptionFieldFilter) SkipEvent(ctx *Context, data []byte, buf *bytes.Buffer) (bool, error) {
+	if f == nil {
+		return false, nil
+	}
+
+	expected, _, _, _err := jsonparser.Get(data, f.FieldPath...)
+	if _err != nil {
+		return true, nil
+	}
+
+	for i := range f.Values {
+		buf.Reset()
+		err := f.Values[i].Render(ctx, nil, buf)
+		if err != nil {
+			return false, err
+		}
+		actual := buf.Bytes()
+		// cheap pre-check to see if we can skip the more expensive array check
+		if !bytes.Contains(actual, literal.LBRACK) {
+			if bytes.Equal(expected, buf.Bytes()) {
+				return false, nil
+			}
+		}
+		if !bytes.Contains(actual, literal.RBRACK) {
+			if bytes.Equal(expected, buf.Bytes()) {
+				return false, nil
+			}
+		}
+		// check if the actual value contains an array, e.g. [1, 2, 3] or ["a", "b", "c"]
+		// if it does, explode the array values into multiple values and compare each one
+		// it's possible that the array is prefixed or suffixed with a non array value, e.g. "foo[1, 2, 3]bar"
+		// so we need to check for that as well
+		// start with a regex to find all array values
+		// then check if the actual value contains the expected value
+		matches := findArray.FindAllSubmatch(actual, -1)
+		if matches == nil {
+			if bytes.Equal(expected, buf.Bytes()) {
+				return false, nil
+			}
+			continue
+		}
+		if len(matches) != 1 || len(matches[0]) != 2 {
+			return false, InvalidSubscriptionFilterTemplate
+		}
+		arrayValue := matches[0][0]
+		arrayMatch := false
+		_, _ = jsonparser.ArrayEach(arrayValue, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			replaced := bytes.Replace(actual, matches[0][0], value, 1)
+			if bytes.Equal(expected, replaced) {
+				arrayMatch = true
+			}
+		})
+		if arrayMatch {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
---
+++ v2/pkg/engine/resolve/subscription_filter_test.go
@@ -0,0 +1,436 @@
+package resolve
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubscriptionFilter(t *testing.T) {
+	t.Run("in allow", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			In: &SubscriptionFieldFilter{
+				FieldPath: []string{"event"},
+				Values: []InputTemplate{
+					{
+						Segments: []TemplateSegment{
+							{
+								SegmentType:        VariableSegmentType,
+								VariableKind:       ContextVariableKind,
+								VariableSourcePath: []string{"var"},
+								Renderer:           NewPlainVariableRenderer(),
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"var":"b"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"event":"b"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, false, skip)
+	})
+	t.Run("in skip", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			In: &SubscriptionFieldFilter{
+				FieldPath: []string{"event"},
+				Values: []InputTemplate{
+					{
+						Segments: []TemplateSegment{
+							{
+								SegmentType:        VariableSegmentType,
+								VariableKind:       ContextVariableKind,
+								VariableSourcePath: []string{"var"},
+								Renderer:           NewPlainVariableRenderer(),
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"var":"b"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"event":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+	t.Run("in array skip", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			In: &SubscriptionFieldFilter{
+				FieldPath: []string{"event"},
+				Values: []InputTemplate{
+					{
+						Segments: []TemplateSegment{
+							{
+								SegmentType:        VariableSegmentType,
+								VariableKind:       ContextVariableKind,
+								VariableSourcePath: []string{"var"},
+								Renderer:           NewPlainVariableRenderer(),
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"var":["a","b"]}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"event":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+	t.Run("in array allow", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			In: &SubscriptionFieldFilter{
+				FieldPath: []string{"event"},
+				Values: []InputTemplate{
+					{
+						Segments: []TemplateSegment{
+							{
+								SegmentType:        VariableSegmentType,
+								VariableKind:       ContextVariableKind,
+								VariableSourcePath: []string{"var"},
+								Renderer:           NewPlainVariableRenderer(),
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"var":["a","b","c"]}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"event":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, false, skip)
+	})
+	t.Run("not in skip", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			Not: &SubscriptionFilter{
+				In: &SubscriptionFieldFilter{
+					FieldPath: []string{"event"},
+					Values: []InputTemplate{
+						{
+							Segments: []TemplateSegment{
+								{
+									SegmentType:        VariableSegmentType,
+									VariableKind:       ContextVariableKind,
+									VariableSourcePath: []string{"var"},
+									Renderer:           NewPlainVariableRenderer(),
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"var":"b"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"event":"b"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+	t.Run("and allow", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			And: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"first":"b","second":"c"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, false, skip)
+	})
+	t.Run("and skip", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			And: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"first":"b","second":"d"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+	t.Run("and skip 2", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			And: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"first":"b","third":"c"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+	t.Run("or allow", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			Or: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"first":"b","second":"c"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, false, skip)
+	})
+	t.Run("or allow differing", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			Or: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"first":"b","third":"c"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, false, skip)
+	})
+	t.Run("or skip", func(t *testing.T) {
+		filter := &SubscriptionFilter{
+			Or: []*SubscriptionFilter{
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventX"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"first"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					In: &SubscriptionFieldFilter{
+						FieldPath: []string{"eventY"},
+						Values: []InputTemplate{
+							{
+								Segments: []TemplateSegment{
+									{
+										SegmentType:        VariableSegmentType,
+										VariableKind:       ContextVariableKind,
+										VariableSourcePath: []string{"second"},
+										Renderer:           NewPlainVariableRenderer(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		c := &Context{
+			Variables: []byte(`{"fourth":"b","third":"c"}`),
+		}
+		buf := &bytes.Buffer{}
+		data := []byte(`{"eventX":"b","eventY":"c"}`)
+		skip, err := filter.SkipEvent(c, data, buf)
+		assert.NoError(t, err)
+		assert.Equal(t, true, skip)
+	})
+}
