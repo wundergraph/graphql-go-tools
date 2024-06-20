0363d20d99a1ced9b7933790bd9ea31a2689304a
Jens Neuse <jens.neuse@gmx.de>
2024-06-20 15:32:04 +0200

    chore: update minify

--- v2/pkg/astminify/minify.go
+++ v2/pkg/astminify/minify.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"hash"
 
 	"github.com/cespare/xxhash/v2"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/ast"
@@ -59,12 +60,16 @@ func (m *Minifier) Minify(options MinifyOptions) (string, error) {
 
 	walker := astvisitor.Walker{}
 	v := &minifyVisitor{
-		w:    &walker,
-		out:  m.out,
-		temp: m.temp,
-		s:    make(map[uint64]*stats),
+		w:         &walker,
+		out:       m.out,
+		temp:      m.temp,
+		s:         make(map[uint64]*stats),
+		buf:       &bytes.Buffer{},
+		h:         xxhash.New(),
+		threshold: options.Threshold,
 	}
 	walker.RegisterEnterSelectionSetVisitor(v)
+	walker.RegisterEnterFragmentDefinitionVisitor(v)
 	report := &operationreport.Report{}
 	walker.Walk(m.out, m.def, report)
 	if report.HasErrors() {
@@ -92,12 +97,12 @@ func (m *Minifier) setupAst() {
 
 func (m *Minifier) apply(vis *minifyVisitor) {
 	for _, s := range vis.s {
-		if s.count > 1 && s.size > m.opts.Threshold {
+		if s.count > 1 {
 			content := string(s.content)
 			if len(content) > 100 {
 				content = content[:100] + "..."
 			}
-			fmt.Printf("SelectionSet with %d occurences, size: %d, content: %s\n\n", s.count, s.size, content)
+			//fmt.Printf("SelectionSet with %d occurences, size: %d, content: %s\n\n", s.count, s.size, content)
 			m.replaceItems(s)
 		}
 	}
@@ -173,7 +178,12 @@ func (m *Minifier) fragmentName() string {
 	chars := make([]byte, 2)
 	chars[0] = alphabet[m.fragmentDefinitionCount/26]
 	chars[1] = alphabet[m.fragmentDefinitionCount%26]
-	return string(chars)
+	name := string(chars)
+	_, exists := m.out.FragmentDefinitionRef([]byte(name))
+	if exists {
+		return m.fragmentName()
+	}
+	return name
 }
 
 type minifyVisitor struct {
@@ -181,7 +191,16 @@ type minifyVisitor struct {
 	out  *ast.Document
 	temp *ast.Document
 
-	s map[uint64]*stats
+	s         map[uint64]*stats
+	threshold int
+
+	buf *bytes.Buffer
+	h   hash.Hash64
+}
+
+func (m *minifyVisitor) EnterFragmentDefinition(ref int) {
+	m.w.SkipNode()
+	panic("implement me")
 }
 
 type stats struct {
@@ -199,29 +218,56 @@ type item struct {
 
 func (m *minifyVisitor) EnterSelectionSet(ref int) {
 
+	ancestor := m.w.Ancestor()
+	if ancestor.Kind == ast.NodeKindFragmentDefinition {
+		return
+	}
+
+	/*hasNestedSelections := false
+
+	for _, i := range m.out.SelectionSets[ref].SelectionRefs {
+		if m.out.Selections[i].Kind == ast.SelectionKindField {
+			if m.out.Fields[m.out.Selections[i].Ref].HasSelections {
+				hasNestedSelections = true
+				break
+			}
+		}
+	}
+
+	if !hasNestedSelections {
+		return
+	}
+	*/
+
 	m.temp.OperationDefinitions[0].SelectionSet = ref
 
 	printer := astprinter.NewPrinter(nil)
-	out := &bytes.Buffer{}
-	err := printer.Print(m.temp, nil, out)
+	m.buf.Reset()
+	err := printer.Print(m.temp, nil, m.buf)
 	if err != nil {
 		return
 	}
-	data := out.Bytes()
-	hash := xxhash.Sum64(data)
+	data := m.buf.Bytes()
+	if len(data) < m.threshold {
+		return
+	}
+
+	m.h.Reset()
+	_, _ = m.h.Write(data)
+	key := m.h.Sum64()
 
 	i := item{
 		selectionSet:  ref,
-		ancestor:      m.w.Ancestor(),
+		ancestor:      ancestor,
 		enclosingType: m.w.EnclosingTypeDefinition,
 	}
 
-	if s, ok := m.s[hash]; ok {
+	if s, ok := m.s[key]; ok {
 		s.count++
 		s.items = append(s.items, i)
 		return
 	}
-	m.s[hash] = &stats{
+	m.s[key] = &stats{
 		count:   1,
 		size:    len(data),
 		content: data,
--- v2/pkg/astminify/minify_test.go
+++ v2/pkg/astminify/minify_test.go
@@ -6,8 +6,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/astnormalization"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/asttransform"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/internal/unsafeparser"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/internal/unsafeprinter"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/testing/goldie"
 )
 
@@ -49,4 +52,64 @@ func TestMinifier_Minify(t *testing.T) {
 	fmt.Printf("(run1) originalSize: %d, minifiedSize: %d, compression: %f\n", len(inPrint), len(outPrint), float64(len(outPrint))/float64(len(inPrint)))
 	fmt.Printf("(run2) originalSize: %d, minifiedSize: %d, compression: %f\n", len(outPrint), len(outPrint2), float64(len(outPrint2))/float64(len(outPrint)))
 
+}
+
+func TestMinifier_MinifyMultipleRuns(t *testing.T) {
+	original, err := os.ReadFile("cosmo.graphql")
+	assert.NoError(t, err)
+
+	schema, err := os.ReadFile("tsb-us-in.graphql")
+	assert.NoError(t, err)
+
+	definition := unsafeparser.ParseGraphqlDocumentBytes(schema)
+	err = asttransform.MergeDefinitionWithBaseSchema(&definition)
+	assert.NoError(t, err)
+
+	previousRun := 1
+
+	for i := 0; i < 10; i++ {
+		m, err := NewMinifier(string(original), string(schema))
+		assert.NoError(t, err)
+		opts := MinifyOptions{
+			Pretty:    true,
+			Threshold: 12,
+		}
+		minified, err := m.Minify(opts)
+		assert.NoError(t, err)
+
+		assert.NoError(t, err)
+		goldie.Assert(t, fmt.Sprintf("cosmo.min.run-%d.graphql", i), []byte(minified))
+
+		norm := astnormalization.NewWithOpts(
+			astnormalization.WithInlineFragmentSpreads(),
+		)
+
+		report := &operationreport.Report{}
+
+		in := unsafeparser.ParseGraphqlDocumentString(string(original))
+		norm.NormalizeNamedOperation(&in, &definition, []byte(`MyQuery`), report)
+		if report.HasErrors() {
+			t.Fatal(report.Error())
+		}
+
+		inPrint := unsafeprinter.Print(&in, &definition)
+		out := unsafeparser.ParseGraphqlDocumentString(minified)
+		outPrint := unsafeprinter.Print(&out, &definition)
+
+		norm.NormalizeNamedOperation(&out, &definition, []byte(`MyQuery`), report)
+		if report.HasErrors() {
+			t.Fatal(report.Error())
+		}
+
+		outPrintNormalized := unsafeprinter.Print(&out, &definition)
+		assert.Equal(t, outPrint, outPrintNormalized)
+
+		if previousRun != 1 && len(outPrint) >= previousRun {
+			fmt.Printf("(run: %d) no more compression possible\n", i)
+			break
+		}
+
+		fmt.Printf("(run: %d) originalSize: %d, minifiedSize: %d, compression: %f improvement: %f\n", i, len(inPrint), len(outPrint), float64(len(outPrint))/float64(len(inPrint)), float64(len(outPrint))/float64(previousRun))
+		previousRun = len(outPrint)
+	}
 }
