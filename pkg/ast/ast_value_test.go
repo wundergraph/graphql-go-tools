6ea0520a5242d2b66d0b62527758a45170132688
Matt Johnson-Pint <mjp@hypermode.com>
2024-07-02 18:31:48 -0700

    Fix in V1 also

--- pkg/ast/ast_value_test.go
+++ pkg/ast/ast_value_test.go
@@ -93,15 +93,43 @@ func TestDocument_ValueToJSON(t *testing.T) {
 		}
 	}, `"foo\nbar\tbaz\"qux"`))
 	t.Run("ValueKindString - block", run(func(doc *Document) Value {
+		doc.Input.AppendInputString(`"""`)
 		doc.StringValues = append(doc.StringValues, StringValue{
 			BlockString: true,
 			Content:     doc.Input.AppendInputString("foo\nbar\tbaz\"qux"),
 		})
+		doc.Input.AppendInputString(`"""`)
 		return Value{
 			Kind: ValueKindString,
 			Ref:  0,
 		}
 	}, `"foo\nbar\tbaz\"qux"`))
+	t.Run("ValueKindString - block with indent", run(func(doc *Document) Value {
+		doc.Input.AppendInputString(`"""`)
+		doc.Input.AppendInputString("\n")
+		doc.StringValues = append(doc.StringValues, StringValue{
+			BlockString: true,
+			Content:     doc.Input.AppendInputString("  foo\n  bar"),
+		})
+		doc.Input.AppendInputString("\n")
+		doc.Input.AppendInputString(`"""`)
+		return Value{
+			Kind: ValueKindString,
+			Ref:  0,
+		}
+	}, `"foo\nbar"`))
+	t.Run("ValueKindString - block with mixed indent", run(func(doc *Document) Value {
+		doc.Input.AppendInputString(`"""`)
+		doc.StringValues = append(doc.StringValues, StringValue{
+			BlockString: true,
+			Content:     doc.Input.AppendInputString("foo\n\t bar\n\t  baz"),
+		})
+		doc.Input.AppendInputString(`"""`)
+		return Value{
+			Kind: ValueKindString,
+			Ref:  0,
+		}
+	}, `"foo\nbar\n baz"`))
 	t.Run("ValueKindList", run(func(doc *Document) Value {
 		doc.StringValues = append(doc.StringValues, StringValue{
 			Content: doc.Input.AppendInputString("foo"),
