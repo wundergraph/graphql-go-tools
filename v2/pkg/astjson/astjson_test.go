6233a7bb430caa9af65fa2bae495ceeb6ef9936c
Jens Neuse <jens.neuse@gmx.de>
2024-07-17 13:09:36 +0200

    chore: cleanup

--- v2/pkg/astjson/astjson_test.go
+++ v2/pkg/astjson/astjson_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/buger/jsonparser"
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fastjson"
-	"github.com/wundergraph/graphql-go-tools/v2/pkg/internal/unsafebytes"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/fastjsonext"
 )
 
 func TestJSON_ParsePrint(t *testing.T) {
@@ -424,86 +424,12 @@ func BenchmarkFastJSON(b *testing.B) {
 	}
 }
 
-func fastJsonMerge(a, b *fastjson.Value) (*fastjson.Value, bool) {
-	if a == nil {
-		return b, true
-	}
-	if b == nil {
-		return a, false
-	}
-	if a.Type() != b.Type() {
-		return a, false
-	}
-	switch a.Type() {
-	case fastjson.TypeObject:
-		ao, _ := a.Object()
-		bo, _ := b.Object()
-		ao.Visit(func(key []byte, l *fastjson.Value) {
-			sKey := unsafebytes.BytesToString(key)
-			r := bo.Get(sKey)
-			if r == nil {
-				return
-			}
-			merged, changed := fastJsonMerge(l, r)
-			if changed {
-				ao.Set(unsafebytes.BytesToString(key), merged)
-			}
-		})
-		bo.Visit(func(key []byte, r *fastjson.Value) {
-			sKey := unsafebytes.BytesToString(key)
-			if ao.Get(sKey) != nil {
-				return
-			}
-			ao.Set(sKey, r)
-		})
-		return a, false
-	case fastjson.TypeArray:
-		aa, _ := a.Array()
-		ba, _ := b.Array()
-		for i := 0; i < len(ba); i++ {
-			a.SetArrayItem(len(aa)+i, ba[i])
-		}
-		return a, false
-	case fastjson.TypeFalse:
-		if b.Type() == fastjson.TypeTrue {
-			return b, true
-		}
-		return a, false
-	case fastjson.TypeTrue:
-		if b.Type() == fastjson.TypeFalse {
-			return b, true
-		}
-		return a, false
-	case fastjson.TypeNull:
-		if b.Type() != fastjson.TypeNull {
-			return b, true
-		}
-		return a, false
-	case fastjson.TypeNumber:
-		af, _ := a.Float64()
-		bf, _ := b.Float64()
-		if af != bf {
-			return b, true
-		}
-		return a, false
-	case fastjson.TypeString:
-		as, _ := a.StringBytes()
-		bs, _ := b.StringBytes()
-		if !bytes.Equal(as, bs) {
-			return b, true
-		}
-		return a, false
-	default:
-		return b, true
-	}
-}
-
 func TestFastJsonMerge(t *testing.T) {
 	a, err := fastjson.ParseBytes([]byte(`{"a":1,"b":2}`))
 	assert.NoError(t, err)
 	b, err := fastjson.ParseBytes([]byte(`{"c":3}`))
 	assert.NoError(t, err)
-	merged, _ := fastJsonMerge(a, b)
+	merged, _ := fastjsonext.MergeValues(a, b)
 	out := merged.MarshalTo(nil)
 	assert.Equal(t, `{"a":1,"b":2,"c":3}`, string(out))
 }
@@ -513,7 +439,7 @@ func TestFastJsonMergeNested(t *testing.T) {
 	assert.NoError(t, err)
 	b, err := fastjson.ParseBytes([]byte(`{"c":{"e":5}}`))
 	assert.NoError(t, err)
-	merged, _ := fastJsonMerge(a, b)
+	merged, _ := fastjsonext.MergeValues(a, b)
 	out := merged.MarshalTo(nil)
 	assert.Equal(t, `{"a":1,"b":2,"c":{"d":4,"e":5}}`, string(out))
 }
@@ -577,8 +503,8 @@ func BenchmarkFastJsonMerge(t *testing.B) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		ab, _ := fastJsonMerge(a, b)
-		abc, _ := fastJsonMerge(ab, c)
+		ab, _ := fastjsonext.MergeValues(a, b)
+		abc, _ := fastjsonext.MergeValues(ab, c)
 		out = abc.MarshalTo(out[:0])
 		if !bytes.Equal(expected, out) {
 			t.Fatal("not equal")
