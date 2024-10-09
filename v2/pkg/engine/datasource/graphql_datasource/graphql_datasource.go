6b7bd6311ab1dc9806bcdbb4fb122e8bd540bb2b
Dave Freilich <dave@wundergraph.com>
2024-10-09 16:40:38 +0300

    fix: remove unnecessary SendHeartbeat property

--- v2/pkg/engine/datasource/graphql_datasource/graphql_datasource.go
+++ v2/pkg/engine/datasource/graphql_datasource/graphql_datasource.go
@@ -1743,7 +1743,6 @@ type GraphQLSubscriptionOptions struct {
 	Header                                  http.Header      `json:"header"`
 	UseSSE                                  bool             `json:"use_sse"`
 	SSEMethodPost                           bool             `json:"sse_method_post"`
-	SendHeartbeat                           bool             `json:"send_heartbeat"`
 	ForwardedClientHeaderNames              []string         `json:"forwarded_client_header_names"`
 	ForwardedClientHeaderRegularExpressions []*regexp.Regexp `json:"forwarded_client_header_regular_expressions"`
 	WsSubProtocol                           string           `json:"ws_sub_protocol"`
@@ -1770,7 +1769,6 @@ func (s *SubscriptionSource) Start(ctx *resolve.Context, input []byte, updater r
 	if options.Body.Query == "" {
 		return resolve.ErrUnableToResolve
 	}
-	options.SendHeartbeat = ctx.ExecutionOptions.SendHeartbeat
 	return s.client.Subscribe(ctx, options, updater)
 }
 
