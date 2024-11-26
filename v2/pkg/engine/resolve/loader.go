438b0fa9077e600102f89ef3a4de0107876dd617
Jens Neuse <jens.neuse@gmx.de>
2024-11-26 12:11:20 +0100

    chore: fix lint

--- v2/pkg/engine/resolve/loader.go
+++ v2/pkg/engine/resolve/loader.go
@@ -207,8 +207,7 @@ func (l *Loader) resolveSingle(item *FetchItem) error {
 				out: f.GetBuffer(),
 			}
 			if l.ctx.TracingOptions.Enable {
-				f.Traces[i] = new(SingleFetch)
-				*f.Traces[i] = *f.Fetch
+				f.Traces[i] = f.Fetch
 				g.Go(func() error {
 					return l.loadFetch(ctx, f.Traces[i], item, items[i:i+1], results[i])
 				})
