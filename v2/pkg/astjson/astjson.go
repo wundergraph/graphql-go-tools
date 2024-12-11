3d23211f510cd061694e603081e47d52c26418b4
Jens Neuse <jens.neuse@gmx.de>
2024-12-11 09:23:54 +0100

    chore: fix lint

--- v2/pkg/astjson/astjson.go
+++ v2/pkg/astjson/astjson.go
@@ -42,6 +42,7 @@ func (p *pool) Put(j *JSON) {
 	p.p.Put(j)
 }
 
+// lint:ignore
 // Deprecated: JSON is deprecated, use the github.com/wundergraph/astjson package instead
 type JSON struct {
 	storage      []byte
