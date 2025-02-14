3ce802feb93111ba0e737009340abf336497477b
endigma <endigma@mailcat.ca>
2025-02-14 10:02:11 +0000

    style: separate errorcodes for gateway and router compat

--- v2/pkg/errorcodes/const.go
+++ v2/pkg/errorcodes/const.go
@@ -1,9 +1,12 @@
 package errorcodes
 
 const (
-	BadUserInput                  = "BAD_USER_INPUT"
-	GraphQLValidationFailed       = "GRAPHQL_VALIDATION_FAILED"
-	InternalServerError           = "INTERNAL_SERVER_ERROR"
-	InvalidGraphql                = "INVALID_GRAPHQL"
+	// Apollo Gateway Compatibility
+	BadUserInput            = "BAD_USER_INPUT"
+	GraphQLValidationFailed = "GRAPHQL_VALIDATION_FAILED"
+	InternalServerError     = "INTERNAL_SERVER_ERROR"
+	InvalidGraphql          = "INVALID_GRAPHQL"
+
+	// Apollo Router Compatibility
 	ValidationInvalidTypeVariable = "VALIDATION_INVALID_TYPE_VARIABLE"
 )
