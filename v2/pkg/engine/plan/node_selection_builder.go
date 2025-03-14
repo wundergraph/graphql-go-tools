1a16d0ead464cdfbf723b2dfc701bb1408dea249
spetrunin <neyasut@gmail.com>
2025-03-14 18:03:24 +0200

    chore: move select nodes and planning paths to node selection builder and path builder

---
+++ v2/pkg/engine/plan/node_selection_builder.go
@@ -0,0 +1,180 @@
+package plan
+
+import (
+	"fmt"
+
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/ast"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/astprinter"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/astvisitor"
+	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
+)
+
+type NodeSelectionBuilder struct {
+	config *Configuration
+
+	nodeResolvableWalker  *astvisitor.Walker
+	nodeResolvableVisitor *nodesResolvableVisitor
+	nodeSelectionsWalker  *astvisitor.Walker
+	nodeSelectionsVisitor *nodeSelectionVisitor
+}
+
+type NodeSelectionResult struct {
+	dataSources              []DataSource                                     // data sources configurations, which used by the current operation
+	nodeSuggestions          *NodeSuggestions                                 // nodeSuggestions holds information about suggested data sources for each field
+	fieldDependsOn           map[fieldIndexKey][]int                          // fieldDependsOn is a map[fieldIndexKey][]fieldRef - holds list of field refs which are required by a field ref, e.g. field should be planned only after required fields were planned
+	fieldRequirementsConfigs map[fieldIndexKey][]FederationFieldConfiguration // fieldRequirementsConfigs is a map[fieldIndexKey]FederationFieldConfiguration - holds a list of required configuratuibs for a field ref to later built representation variables
+	skipFieldsRefs           []int                                            // skipFieldsRefs holds required field refs added by planner and should not be added to user response
+}
+
+func NewNodeSelectionBuilder(config *Configuration, operationName string) *NodeSelectionBuilder {
+	nodeSelectionsWalker := astvisitor.NewWalker(48)
+	nodeSelectionVisitor := &nodeSelectionVisitor{
+		walker:        &nodeSelectionsWalker,
+		operationName: operationName, // TODO: check should not be needed
+	}
+
+	nodeSelectionsWalker.RegisterEnterDocumentVisitor(nodeSelectionVisitor)
+	nodeSelectionsWalker.RegisterFieldVisitor(nodeSelectionVisitor)
+	nodeSelectionsWalker.RegisterEnterOperationVisitor(nodeSelectionVisitor)
+	nodeSelectionsWalker.RegisterSelectionSetVisitor(nodeSelectionVisitor)
+
+	nodeResolvableWalker := astvisitor.NewWalker(32)
+	nodeResolvableVisitor := &nodesResolvableVisitor{
+		walker: &nodeResolvableWalker,
+	}
+	nodeResolvableWalker.RegisterEnterDocumentVisitor(nodeResolvableVisitor)
+	nodeResolvableWalker.RegisterEnterFieldVisitor(nodeResolvableVisitor)
+
+	return &NodeSelectionBuilder{
+		config:                config,
+		nodeSelectionsWalker:  &nodeSelectionsWalker,
+		nodeSelectionsVisitor: nodeSelectionVisitor,
+		nodeResolvableWalker:  &nodeResolvableWalker,
+		nodeResolvableVisitor: nodeResolvableVisitor,
+	}
+}
+
+func (p *NodeSelectionBuilder) SelectNodes(operation, definition *ast.Document, report *operationreport.Report) (out *NodeSelectionResult) {
+	dsFilter := NewDataSourceFilter(operation, definition, report)
+
+	if p.config.Debug.NodeSuggestion.SelectionReasons {
+		dsFilter.EnableSelectionReasons()
+	}
+
+	if p.config.Debug.PrintOperationTransformations {
+		debugMessage("Initial operation:")
+		p.printOperation(operation)
+	}
+
+	p.nodeSelectionsVisitor.debug = p.config.Debug
+
+	// set initial suggestions and used data sources
+	p.nodeSelectionsVisitor.dataSources, p.nodeSelectionsVisitor.nodeSuggestions =
+		dsFilter.FilterDataSources(p.config.DataSources, nil, nil, nil)
+	if report.HasErrors() {
+		return
+	}
+
+	if p.config.Debug.PrintNodeSuggestions {
+		p.nodeSelectionsVisitor.nodeSuggestions.printNodesWithFilter("\nInitial node suggestions:\n", p.config.Debug.NodeSuggestion.FilterNotSelected)
+	}
+
+	p.nodeSelectionsVisitor.secondaryRun = false
+	p.nodeSelectionsWalker.Walk(operation, definition, report)
+	if report.HasErrors() {
+		return
+	}
+
+	if p.config.Debug.PrintOperationTransformations {
+		debugMessage("Select nodes initial run - operation:")
+		p.printOperation(operation)
+	}
+
+	i := 1
+	// secondary runs to add path for the new required fields
+	for p.nodeSelectionsVisitor.shouldRevisit() {
+
+		// when we have rewritten a field old node suggestion are not make sense anymore
+		// so we are removing child nodes of the rewritten fields
+		for _, fieldRef := range p.nodeSelectionsVisitor.rewrittenFieldRefs {
+			p.nodeSelectionsVisitor.nodeSuggestions.RemoveTreeNodeChilds(fieldRef)
+		}
+
+		p.nodeSelectionsVisitor.secondaryRun = true
+
+		if p.nodeSelectionsVisitor.hasNewFields {
+			// update suggestions for the new required fields
+			p.nodeSelectionsVisitor.dataSources, p.nodeSelectionsVisitor.nodeSuggestions =
+				dsFilter.FilterDataSources(p.config.DataSources, p.nodeSelectionsVisitor.nodeSuggestions, p.nodeSelectionsVisitor.fieldLandedTo, p.nodeSelectionsVisitor.fieldRefDependsOn)
+			if report.HasErrors() {
+				return
+			}
+		}
+
+		if p.config.Debug.PrintOperationTransformations || p.config.Debug.PrintNodeSuggestions {
+			debugMessage(fmt.Sprintf("Select nodes run #%d", i))
+		}
+
+		if p.config.Debug.PrintNodeSuggestions {
+			p.nodeSelectionsVisitor.nodeSuggestions.printNodesWithFilter("\nRecalculated node suggestions:\n", p.config.Debug.NodeSuggestion.FilterNotSelected)
+		}
+
+		p.nodeSelectionsWalker.Walk(operation, definition, report)
+		if report.HasErrors() {
+			return
+		}
+
+		if p.config.Debug.PrintOperationTransformations {
+			debugMessage("Operation with new required fields:")
+			debugMessage(fmt.Sprintf("Has new fields: %v", p.nodeSelectionsVisitor.hasNewFields))
+			p.printOperation(operation)
+		}
+
+		i++
+
+		if resolvableReport := p.isResolvable(operation, definition, p.nodeSelectionsVisitor.nodeSuggestions); resolvableReport.HasErrors() {
+			p.nodeSelectionsVisitor.hasUnresolvedFields = true
+
+			if i > 100 {
+				report.AddInternalError(fmt.Errorf("could not resolve a field: %v", resolvableReport))
+				return
+			}
+		}
+	}
+
+	if i == 1 {
+		// if we have not revisited the operation, we need to check if it is resolvable
+		if resolvableReport := p.isResolvable(operation, definition, p.nodeSelectionsVisitor.nodeSuggestions); resolvableReport.HasErrors() {
+			p.nodeSelectionsVisitor.hasUnresolvedFields = true
+			report.AddInternalError(fmt.Errorf("could not resolve a field: %v", resolvableReport))
+		}
+	}
+
+	return &NodeSelectionResult{
+		dataSources:              p.nodeSelectionsVisitor.dataSources,
+		nodeSuggestions:          p.nodeSelectionsVisitor.nodeSuggestions,
+		fieldDependsOn:           p.nodeSelectionsVisitor.fieldDependsOn,
+		fieldRequirementsConfigs: p.nodeSelectionsVisitor.fieldRequirementsConfigs,
+		skipFieldsRefs:           p.nodeSelectionsVisitor.skipFieldsRefs,
+	}
+}
+
+func (p *NodeSelectionBuilder) isResolvable(operation, definition *ast.Document, nodes *NodeSuggestions) *operationreport.Report {
+	p.nodeResolvableVisitor.nodes = nodes
+	resolvableReport := &operationreport.Report{}
+	p.nodeResolvableWalker.Walk(operation, definition, resolvableReport)
+
+	return resolvableReport
+}
+
+func (p *NodeSelectionBuilder) printOperation(operation *ast.Document) {
+	var pp string
+
+	if p.config.Debug.PrintOperationEnableASTRefs {
+		pp, _ = astprinter.PrintStringIndentDebug(operation, "  ")
+	} else {
+		pp, _ = astprinter.PrintStringIndent(operation, "  ")
+	}
+
+	fmt.Println(pp)
+}
--- v2/pkg/engine/plan/planner.go
+++ v2/pkg/engine/plan/planner.go
@@ -8,21 +8,15 @@ import (
 
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/ast"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/astnormalization"
-	"github.com/wundergraph/graphql-go-tools/v2/pkg/astprinter"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/astvisitor"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
 )
 
 type Planner struct {
-	config                Configuration
-	nodeResolvableWalker  *astvisitor.Walker
-	nodeResolvableVisitor *nodesResolvableVisitor
-	nodeSelectionsWalker  *astvisitor.Walker
-	nodeSelectionsVisitor *nodeSelectionVisitor
-	configurationWalker   *astvisitor.Walker
-	configurationVisitor  *configurationVisitor
-	planningWalker        *astvisitor.Walker
-	planningVisitor       *Visitor
+	config Configuration
+
+	planningWalker  *astvisitor.Walker
+	planningVisitor *Visitor
 
 	prepareOperationWalker *astvisitor.Walker
 }
@@ -56,29 +50,6 @@ func NewPlanner(config Configuration) (*Planner, error) {
 	prepareOperationWalker := astvisitor.NewWalker(48)
 	astnormalization.InlineFragmentAddOnType(&prepareOperationWalker)
 
-	// node selection
-	nodeSelection := astvisitor.NewWalker(48)
-	nodeSelectionVisitor := &nodeSelectionVisitor{
-		walker: &nodeSelection,
-	}
-
-	nodeSelection.RegisterEnterDocumentVisitor(nodeSelectionVisitor)
-	nodeSelection.RegisterFieldVisitor(nodeSelectionVisitor)
-	nodeSelection.RegisterEnterOperationVisitor(nodeSelectionVisitor)
-	nodeSelection.RegisterSelectionSetVisitor(nodeSelectionVisitor)
-
-	// configuration
-	configurationWalker := astvisitor.NewWalker(48)
-	configVisitor := &configurationVisitor{
-		walker:              &configurationWalker,
-		fieldConfigurations: config.Fields,
-	}
-
-	configurationWalker.RegisterEnterDocumentVisitor(configVisitor)
-	configurationWalker.RegisterFieldVisitor(configVisitor)
-	configurationWalker.RegisterEnterOperationVisitor(configVisitor)
-	configurationWalker.RegisterSelectionSetVisitor(configVisitor)
-
 	// planning
 
 	planningWalker := astvisitor.NewWalker(48)
@@ -90,10 +61,6 @@ func NewPlanner(config Configuration) (*Planner, error) {
 
 	p := &Planner{
 		config:                 config,
-		configurationWalker:    &configurationWalker,
-		configurationVisitor:   configVisitor,
-		nodeSelectionsWalker:   &nodeSelection,
-		nodeSelectionsVisitor:  nodeSelectionVisitor,
 		planningWalker:         &planningWalker,
 		planningVisitor:        planningVisitor,
 		prepareOperationWalker: &prepareOperationWalker,
@@ -123,7 +90,6 @@ func IncludeQueryPlanInResponse() Opts {
 }
 
 func (p *Planner) Plan(operation, definition *ast.Document, operationName string, report *operationreport.Report, options ...Opts) (plan Plan) {
-	p.nodeSelectionsVisitor.skipFieldsRefs = nil
 
 	var opts _opts
 	for _, opt := range options {
@@ -147,20 +113,29 @@ func (p *Planner) Plan(operation, definition *ast.Document, operationName string
 		p.config.DataSources[i].Hash()
 	}
 
-	p.findPlanningPaths(operation, definition, report)
+	// create node selections
+	nodeSelectionBuilder := NewNodeSelectionBuilder(&p.config, operationName)
+	selectionsConfig := nodeSelectionBuilder.SelectNodes(operation, definition, report)
+	if report.HasErrors() {
+		return nil
+	}
+
+	// create planning paths
+	planningPathBuilder := NewPathBuilder(&p.config, selectionsConfig, operationName)
+	plannersConfigurations := planningPathBuilder.CreatePlanningPaths(operation, definition, report)
 	if report.HasErrors() {
 		return nil
 	}
 
 	if p.config.Debug.PlanningVisitor {
-		p.debugMessage("Planning visitor:")
+		debugMessage("Planning visitor:")
 	}
 
 	// configure planning visitor
 
-	p.planningVisitor.planners = p.configurationVisitor.planners
+	p.planningVisitor.planners = plannersConfigurations
 	p.planningVisitor.Config = p.config
-	p.planningVisitor.skipFieldsRefs = p.nodeSelectionsVisitor.skipFieldsRefs
+	p.planningVisitor.skipFieldsRefs = selectionsConfig.skipFieldsRefs
 
 	p.planningWalker.ResetVisitors()
 	p.planningWalker.SetVisitorFilter(p.planningVisitor)
@@ -201,236 +176,13 @@ func (p *Planner) Plan(operation, definition *ast.Document, operationName string
 		}
 	}
 
-	// process the plan
-
+	// create raw execution plan
 	p.planningWalker.Walk(operation, definition, report)
 	if report.HasErrors() {
 		return
 	}
 
 	return p.planningVisitor.plan
-}
-
-func (p *Planner) findPlanningPaths(operation, definition *ast.Document, report *operationreport.Report) {
-	p.selectNodes(operation, definition, report)
-	if report.HasErrors() {
-		return
-	}
-
-	p.createPlanningPaths(operation, definition, report)
-}
-
-func (p *Planner) selectNodes(operation, definition *ast.Document, report *operationreport.Report) {
-	dsFilter := NewDataSourceFilter(operation, definition, report)
-
-	if p.config.Debug.NodeSuggestion.SelectionReasons {
-		dsFilter.EnableSelectionReasons()
-	}
-
-	if p.config.Debug.PrintOperationTransformations {
-		p.debugMessage("Initial operation:")
-		p.printOperation(operation)
-	}
-
-	if p.config.MaxDataSourceCollectorsConcurrency > 0 {
-		dsFilter.WithMaxDataSourceCollectorsConcurrency(p.config.MaxDataSourceCollectorsConcurrency)
-	}
-
-	p.nodeSelectionsVisitor.debug = p.config.Debug
-
-	// set initial suggestions and used data sources
-	p.nodeSelectionsVisitor.dataSources, p.nodeSelectionsVisitor.nodeSuggestions =
-		dsFilter.FilterDataSources(p.config.DataSources, nil, nil, nil)
-	if report.HasErrors() {
-		return
-	}
-
-	if p.config.Debug.PrintNodeSuggestions {
-		p.nodeSelectionsVisitor.nodeSuggestions.printNodesWithFilter("\nInitial node suggestions:\n", p.config.Debug.NodeSuggestion.FilterNotSelected)
-	}
-
-	p.nodeSelectionsVisitor.secondaryRun = false
-	p.nodeSelectionsWalker.Walk(operation, definition, report)
-	if report.HasErrors() {
-		return
-	}
-
-	if p.config.Debug.PrintOperationTransformations {
-		p.debugMessage("Select nodes initial run - operation:")
-		p.printOperation(operation)
-	}
-
-	i := 1
-	// secondary runs to add path for the new required fields
-	for p.nodeSelectionsVisitor.shouldRevisit() {
-
-		// when we have rewritten a field old node suggestion are not make sense anymore
-		// so we are removing child nodes of the rewritten fields
-		for _, fieldRef := range p.nodeSelectionsVisitor.rewrittenFieldRefs {
-			p.nodeSelectionsVisitor.nodeSuggestions.RemoveTreeNodeChilds(fieldRef)
-		}
-
-		p.nodeSelectionsVisitor.secondaryRun = true
-
-		if p.nodeSelectionsVisitor.hasNewFields {
-			// update suggestions for the new required fields
-			p.nodeSelectionsVisitor.dataSources, p.nodeSelectionsVisitor.nodeSuggestions =
-				dsFilter.FilterDataSources(p.config.DataSources, p.nodeSelectionsVisitor.nodeSuggestions, p.nodeSelectionsVisitor.fieldLandedTo, p.nodeSelectionsVisitor.fieldRefDependsOn)
-			if report.HasErrors() {
-				return
-			}
-		}
-
-		if p.config.Debug.PrintOperationTransformations || p.config.Debug.PrintNodeSuggestions {
-			p.debugMessage(fmt.Sprintf("Select nodes run #%d", i))
-		}
-
-		if p.config.Debug.PrintNodeSuggestions {
-			p.nodeSelectionsVisitor.nodeSuggestions.printNodesWithFilter("\nRecalculated node suggestions:\n", p.config.Debug.NodeSuggestion.FilterNotSelected)
-		}
-
-		p.nodeSelectionsWalker.Walk(operation, definition, report)
-		if report.HasErrors() {
-			return
-		}
-
-		if p.config.Debug.PrintOperationTransformations {
-			p.debugMessage("Operation with new required fields:")
-			p.debugMessage(fmt.Sprintf("Has new fields: %v", p.nodeSelectionsVisitor.hasNewFields))
-			p.printOperation(operation)
-		}
-
-		i++
-
-		if resolvableReport := p.isResolvable(operation, definition, p.nodeSelectionsVisitor.nodeSuggestions); resolvableReport.HasErrors() {
-			p.nodeSelectionsVisitor.hasUnresolvedFields = true
-
-			if i > 100 {
-				report.AddInternalError(fmt.Errorf("could not resolve a field: %v", resolvableReport))
-				return
-			}
-		}
-	}
-
-	if i == 1 {
-		// if we have not revisited the operation, we need to check if it is resolvable
-		if resolvableReport := p.isResolvable(operation, definition, p.nodeSelectionsVisitor.nodeSuggestions); resolvableReport.HasErrors() {
-			p.nodeSelectionsVisitor.hasUnresolvedFields = true
-			report.AddInternalError(fmt.Errorf("could not resolve a field: %v", resolvableReport))
-		}
-	}
-}
-
-func (p *Planner) isResolvable(operation, definition *ast.Document, nodes *NodeSuggestions) *operationreport.Report {
-	if p.nodeResolvableWalker == nil {
-		walker := astvisitor.NewWalker(32)
-		p.nodeResolvableWalker = &walker
-
-		visitor := &nodesResolvableVisitor{
-			walker: &walker,
-		}
-		p.nodeResolvableVisitor = visitor
-		walker.RegisterEnterDocumentVisitor(visitor)
-		walker.RegisterEnterFieldVisitor(visitor)
-	}
-
-	p.nodeResolvableVisitor.nodes = nodes
-	resolvableReport := &operationreport.Report{}
-	p.nodeResolvableWalker.Walk(operation, definition, resolvableReport)
-
-	return resolvableReport
-}
-
-func (p *Planner) createPlanningPaths(operation, definition *ast.Document, report *operationreport.Report) {
-	if p.config.Debug.PrintPlanningPaths {
-		p.debugMessage("Create planning paths")
-	}
-
-	p.configurationVisitor.plannerConfiguration = p.config
-
-	// set initial suggestions and used data sources
-	p.configurationVisitor.dataSources, p.configurationVisitor.nodeSuggestions =
-		p.nodeSelectionsVisitor.dataSources, p.nodeSelectionsVisitor.nodeSuggestions
-
-	// set fields dependencies information
-	p.configurationVisitor.fieldDependsOn, p.configurationVisitor.fieldRequirementsConfigs =
-		p.nodeSelectionsVisitor.fieldDependsOn, p.nodeSelectionsVisitor.fieldRequirementsConfigs
-
-	p.configurationVisitor.secondaryRun = false
-	p.configurationWalker.Walk(operation, definition, report)
-	if report.HasErrors() {
-		return
-	}
-	// we have to populate missing paths after the walk
-	p.configurationVisitor.populateMissingPahts()
-
-	// walk ends in 2 cases:
-	// - we have finished visiting document
-	// - walker.Stop was called and visiting was halted
-
-	if p.config.Debug.PrintPlanningPaths {
-		p.debugMessage("Planning paths after initial run")
-		p.printRevisitInfo()
-		p.printPlanningPaths()
-	}
-
-	i := 1
-	// secondary runs to add path for the new required fields
-	for p.configurationVisitor.shouldRevisit() {
-		p.configurationVisitor.secondaryRun = true
-
-		p.configurationWalker.Walk(operation, definition, report)
-		if report.HasErrors() {
-			return
-		}
-		// we have to populate missing paths after the walk
-		p.configurationVisitor.populateMissingPahts()
-
-		if p.config.Debug.PrintPlanningPaths {
-			p.debugMessage(fmt.Sprintf("Create planning paths run #%d", i))
-		}
-
-		if p.config.Debug.PrintPlanningPaths {
-			p.printRevisitInfo()
-			p.printPlanningPaths()
-		}
-
-		i++
-
-		if i > 100 {
-			missingPaths := make([]string, 0, len(p.configurationVisitor.missingPathTracker))
-			for path := range p.configurationVisitor.missingPathTracker {
-				missingPaths = append(missingPaths, path)
-			}
-
-			report.AddInternalError(fmt.Errorf("failed to obtain planning paths: %w", newFailedToCreatePlanningPathsError(
-				missingPaths,
-				p.configurationVisitor.hasFieldsWaitingForDependency(),
-			)))
-			return
-		}
-	}
-
-	// remove unnecessary fragment paths
-	hasRemovedPaths := p.removeUnnecessaryFragmentPaths()
-	if hasRemovedPaths && p.config.Debug.PrintPlanningPaths {
-		p.debugMessage("After removing unnecessary fragment paths")
-		p.printPlanningPaths()
-	}
-}
-
-func (p *Planner) removeUnnecessaryFragmentPaths() (hasRemovedPaths bool) {
-	// We add fragment paths on enter selection set of fragments in configurationVisitor
-	// It could happen that datasource has a root node for the given fragment type,
-	// but we do not select any fields from this fragment
-	// So we need to remove all fragment paths that are not prefixes of any other path
-
-	for _, planner := range p.configurationVisitor.planners {
-		if planner.RemoveLeafFragmentPaths() {
-			hasRemovedPaths = true
-		}
-	}
-	return
 }
 
 func (p *Planner) selectOperation(operation *ast.Document, operationName string, report *operationreport.Report) {
@@ -451,8 +203,6 @@ func (p *Planner) selectOperation(operation *ast.Document, operationName string,
 		return
 	}
 
-	p.configurationVisitor.operationName = operationName
-	p.nodeSelectionsVisitor.operationName = operationName
 	p.planningVisitor.OperationName = operationName
 }
 
@@ -460,60 +210,6 @@ func (p *Planner) prepareOperation(operation, definition *ast.Document, report *
 	p.prepareOperationWalker.Walk(operation, definition, report)
 }
 
-func (p *Planner) printOperation(operation *ast.Document) {
-	var pp string
-
-	if p.config.Debug.PrintOperationEnableASTRefs {
-		pp, _ = astprinter.PrintStringIndentDebug(operation, "  ")
-	} else {
-		pp, _ = astprinter.PrintStringIndent(operation, "  ")
-	}
-
-	fmt.Println(pp)
-}
-
-func (p *Planner) printRevisitInfo() {
-	fmt.Println("Should revisit:", p.configurationVisitor.shouldRevisit())
-	fmt.Println("Has missing paths:", p.configurationVisitor.hasMissingPaths())
-	fmt.Println("Has fields waiting for dependency:", p.configurationVisitor.hasFieldsWaitingForDependency())
-
-	p.printMissingPaths()
-}
-
-func (p *Planner) printPlanningPaths() {
-	p.debugMessage("\n\nPlanning paths:\n\n")
-	for i, planner := range p.configurationVisitor.planners {
-		fmt.Printf("\nPlanner id: %d\n", i)
-		fmt.Printf("Parent path: %s\n", planner.ParentPath())
-		ds := planner.DataSourceConfiguration()
-		fmt.Printf("Datasource id: %s name: %s hash: %d\n", ds.Id(), ds.Name(), ds.Hash())
-		fmt.Printf("Depends on planner ids: %v\n", planner.ObjectFetchConfiguration().dependsOnFetchIDs)
-
-		requiredFields := planner.RequiredFields()
-		if requiredFields != nil && len(*requiredFields) > 0 {
-			fmt.Println("Required fields:")
-			for _, field := range *requiredFields {
-				fmt.Println(field)
-			}
-		}
-		fmt.Println("Paths:")
-		planner.ForEachPath(func(path *pathConfiguration) (shouldBreak bool) {
-			fmt.Println(path.String())
-			return false
-		})
-		fmt.Println()
-	}
-}
-
-func (p *Planner) printMissingPaths() {
-	if p.configurationVisitor.hasMissingPaths() {
-		p.debugMessage("Missing paths:")
-		for path := range p.configurationVisitor.missingPathTracker {
-			fmt.Println(path)
-		}
-	}
-}
-
-func (p *Planner) debugMessage(msg string) {
+func debugMessage(msg string) {
 	fmt.Printf("\n\n%s\n\n", msg)
 }
