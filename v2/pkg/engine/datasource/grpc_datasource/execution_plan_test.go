7c410496baff5ffd7ea37b01525a3789aa127e6d
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-04-03 13:46:06 +0200

    chore: fix rpc plan layout

--- v2/pkg/engine/datasource/grpc_datasource/execution_plan_test.go
+++ v2/pkg/engine/datasource/grpc_datasource/execution_plan_test.go
@@ -1,9 +1,9 @@
 package grpcdatasource
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/ast"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/astparser"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/asttransform"
@@ -11,8 +11,7 @@ import (
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/operationreport"
 )
 
-func TestConstructExecutionPlan(t *testing.T) {
-	schema := `
+var testSchema = `
 	scalar ID
 	scalar String
 	scalar Float
@@ -39,6 +38,8 @@ union _Entity = Product
 scalar _Any
 `
 
+func TestConstructExecutionPlan(t *testing.T) {
+
 	t.Run("Should create an execution plan for an entity lookup", func(t *testing.T) {
 		// GraphQL Query
 		query := `
@@ -52,11 +53,101 @@ query EntityLookup($representations: [_Any!]!) {
 	}
 }
 `
+
+		expectedPlan := &RPCExecutionPlan{
+			Calls: []RPCCall{
+				{
+					ServiceName: "Products",
+					MethodName:  "LookupProductById",
+					// Define the structure of the request message
+					Request: RPCMessage{
+						Name: "LookupProductByIdRequest",
+						Fields: []RPCField{
+							{
+								Name:     "inputs",
+								TypeName: string(DataTypeMessage),
+								Repeated: true,
+								JSONPath: "variables.representations", // Path to extract data from GraphQL variables
+								Index:    0,
+								Message: &RPCMessage{
+									Name: "LookupProductByIdInput",
+									Fields: []RPCField{
+										{
+											Name:     "key",
+											TypeName: string(DataTypeMessage),
+											Index:    0,
+											Message: &RPCMessage{
+												Name: "ProductByIdKey",
+												Fields: []RPCField{
+													{
+														Name:     "id",
+														TypeName: string(DataTypeString),
+														JSONPath: "id", // Extract 'id' from each representation
+														Index:    0,
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					// Define the structure of the response message
+					Response: RPCMessage{
+						Name: "LookupProductByIdResponse",
+						Fields: []RPCField{
+							{
+								Name:     "results",
+								TypeName: string(DataTypeMessage),
+								Repeated: true,
+								Index:    0,
+								JSONPath: "results",
+								Message: &RPCMessage{
+									Name: "LookupProductByIdResult",
+									Fields: []RPCField{
+										{
+											Name:     "product",
+											TypeName: string(DataTypeMessage),
+											Index:    0,
+											Message: &RPCMessage{
+												Name: "Product",
+												Fields: []RPCField{
+													{
+														Name:     "id",
+														TypeName: string(DataTypeString),
+														JSONPath: "id",
+														Index:    0,
+													},
+													{
+														Name:     "name",
+														TypeName: string(DataTypeString),
+														JSONPath: "name",
+														Index:    1,
+													},
+													{
+														Name:     "price",
+														TypeName: string(DataTypeFloat),
+														JSONPath: "price",
+														Index:    2,
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
 		report := &operationreport.Report{}
 
 		// Parse the GraphQL schema
 		schemaDoc := ast.NewDocument()
-		schemaDoc.Input.ResetInputString(schema)
+		schemaDoc.Input.ResetInputString(testSchema)
 		astparser.NewParser().Parse(schemaDoc, report)
 		if report.HasErrors() {
 			t.Fatalf("failed to parse schema: %s", report.Error())
@@ -85,11 +176,14 @@ query EntityLookup($representations: [_Any!]!) {
 			t.Fatalf("failed to walk AST: %s", report.Error())
 		}
 
-		fmt.Println("--------------------------------")
+		diff := cmp.Diff(expectedPlan, rpcPlanVisitor.plan)
+		if diff != "" {
+			t.Fatalf("execution plan mismatch: %s", diff)
+		}
 
-		fmt.Println(rpcPlanVisitor.plan.String())
+		// fmt.Println(rpcPlanVisitor.plan.String())
+		// fmt.Println(expectedPlan.String())
 
-		fmt.Println("--------------------------------")
 	})
 
 }
