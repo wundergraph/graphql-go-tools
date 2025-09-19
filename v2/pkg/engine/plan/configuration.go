df1743fbe7d10fb20bede876e8ee555e5f7e4dfa
Yury Smolski <140245+ysmolski@users.noreply.github.com>
2025-09-19 15:03:12 +0300

    edit the comment

--- v2/pkg/engine/plan/configuration.go
+++ v2/pkg/engine/plan/configuration.go
@@ -40,8 +40,8 @@ type Configuration struct {
 	// It requires DisableIncludeInfo and DisableIncludeFieldDependencies set to false.
 	BuildFetchReasons bool
 
-	// ValidateRequiredExternalFields validates optional external "@requires" dependencies.
-	// When a subgraph returns a null value with an error for optional field set specified in
+	// ValidateRequiredExternalFields validates nullable external "@requires" dependencies.
+	// When a subgraph returns a null value with an error for a field set specified in
 	// the "@requires" directive, any later resolver that depends on it should not receive such an
 	// entity.
 	// This option requires BuildFetchReasons set to true.
