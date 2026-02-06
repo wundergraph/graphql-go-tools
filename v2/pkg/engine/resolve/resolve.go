59089cc443fa7097401d89ad65e77bed087c3baa
Yury Smolski <140245+ysmolski@users.noreply.github.com>
2026-02-06 18:03:16 +0200

    add comment

--- v2/pkg/engine/resolve/resolve.go
+++ v2/pkg/engine/resolve/resolve.go
@@ -318,7 +318,9 @@ type GraphQLResolveInfo struct {
 	ResolveAcquireWaitTime time.Duration
 	// ResolveDeduplicated indicates whether the resolution of the entire operation was deduplicated via single flight
 	ResolveDeduplicated bool
-	ActualListSizes     map[string]int
+	// ActualListSizes is a map of json paths to the actual list size in the response.
+	// Used for calculating the actual costs.
+	ActualListSizes map[string]int
 }
 
 func (r *Resolver) ResolveGraphQLResponse(ctx *Context, response *GraphQLResponse, data []byte, writer io.Writer) (*GraphQLResolveInfo, error) {
