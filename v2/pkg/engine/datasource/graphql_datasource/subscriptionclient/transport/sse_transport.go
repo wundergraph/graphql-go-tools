e9b0c506afa26f12a0fce94434f889b114c3d17a
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2026-06-16 14:42:55 +0200

    fix: remove completed connections from transport connection pool

--- v2/pkg/engine/datasource/graphql_datasource/subscriptionclient/transport/sse_transport.go
+++ v2/pkg/engine/datasource/graphql_datasource/subscriptionclient/transport/sse_transport.go
@@ -130,7 +130,11 @@ func (t *SSETransport) Subscribe(ctx context.Context, req *common.Request, opts 
 	)
 
 	// Create connection
-	conn := newSSEConnection(resp, handler)
+	var conn *sseConnection
+	// Connections receiving a terminal message will be closed but the connection itself will
+	// not be removed from the transport's connection map until the transport is closed.
+	// This ensures to properly clean up stale connections even when the transport is still active.
+	conn = newSSEConnection(resp, handler, func() { t.removeConn(conn) })
 
 	t.mu.Lock()
 	t.conns[conn] = struct{}{}
