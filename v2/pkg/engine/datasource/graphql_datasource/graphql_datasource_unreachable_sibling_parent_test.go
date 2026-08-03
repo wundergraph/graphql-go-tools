9b6bef4ca52ffe2711f02da1ba6885ba3d25294f
spetrunin <neyasut@gmail.com>
2026-08-03 12:49:37 +0300

    fmt

--- v2/pkg/engine/datasource/graphql_datasource/graphql_datasource_unreachable_sibling_parent_test.go
+++ v2/pkg/engine/datasource/graphql_datasource/graphql_datasource_unreachable_sibling_parent_test.go
@@ -3,10 +3,9 @@ package graphql_datasource
 import (
 	"testing"
 
+	. "github.com/wundergraph/graphql-go-tools/v2/pkg/engine/datasourcetesting"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/engine/plan"
 	"github.com/wundergraph/graphql-go-tools/v2/pkg/engine/resolve"
-
-	. "github.com/wundergraph/graphql-go-tools/v2/pkg/engine/datasourcetesting"
 )
 
 // TestUnreachableSiblingParentDuplicate distills a production graph where the
